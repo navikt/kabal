3666353a9a3cc43008a6bed56bc0aa8b98a271c7
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-07-28 11:33:32 +0200

    legger til refresh håndtering

--- server/main.js
+++ server/main.js
@@ -50,7 +50,7 @@ async function startApp() {
       const azureAuthClient = await azure.client();
       const azureOidcStrategy = azure.strategy(azureAuthClient);
       passport.use("azureOidc", azureOidcStrategy);
-      refresh.use(azureOidcStrategy);
+      refresh.use("azureOidc", azureOidcStrategy);
       passport.serializeUser((user, done) => done(null, user));
       passport.deserializeUser((user, done) => done(null, user));
       server.use("/", require("./routes").setup(azureAuthClient));
