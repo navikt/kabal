40582553adc866bf25158966c9d2ebfed7515973
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-09-14 18:13:27 +0200

    Set webpack dev proxy to dev

--- frontend/webpack.config.local.js
+++ frontend/webpack.config.local.js
@@ -1,33 +1,33 @@
-const webpack = require("webpack");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: {
-    main: "./src/index.tsx",
+    main: './src/index.tsx',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.less$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
           {
-            loader: "less-loader",
+            loader: 'less-loader',
             options: {
               lessOptions: {
                 strictMath: true,
@@ -40,9 +40,9 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              publicPath: "/",
+              publicPath: '/',
             },
           },
         ],
@@ -50,39 +50,44 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: "node_modules/pdfjs-dist/build/pdf.worker.js",
-          to: "pdf.worker.js",
+          from: 'node_modules/pdfjs-dist/build/pdf.worker.js',
+          to: 'pdf.worker.js',
         },
       ],
     }),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, "public"),
+    contentBase: path.resolve(__dirname, 'public'),
     hot: true,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 8061,
     historyApiFallback: true,
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        pathRewrite: { "^/api": "" },
+      '/api': {
+        target: 'https://kabal.dev.nav.no',
+        secure: false,
+        changeOrigin: true,
       },
-      "/internal": {
-        target: "http://localhost:8090",
+      '/internal': {
+        target: 'https://kabal.dev.nav.no',
+        secure: false,
+        changeOrigin: true,
       },
-      "/me": {
-        target: "http://localhost:3000",
+      '/me': {
+        target: 'https://kabal.dev.nav.no',
+        secure: false,
+        changeOrigin: true,
       },
     },
   },
