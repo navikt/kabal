14803a4b277cbe5dcc118c04cf9809d07262c2a5
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-09-24 16:05:20 +0200

    OBO token in-memory cache

--- server/src/auth/azure/on-behalf-of.ts
+++ server/src/auth/azure/on-behalf-of.ts
@@ -2,9 +2,22 @@ import { Client, GrantBody } from 'openid-client';
 import { client_id } from '../../config/azure-config';
 import { KABAL_API_CLIENT_ID } from '../../config/config';
 
+const oboCache: Map<string, [string, number]> = new Map();
+
 const scope = `api://${KABAL_API_CLIENT_ID}/.default`;
 
 export const getOnBehalfOfAccessToken = async (authClient: Client, access_token: string): Promise<string> => {
+  const cacheHit = oboCache.get(access_token);
+  if (typeof cacheHit !== 'undefined') {
+    const [cached_obo_access_token, expires_at] = cacheHit;
+
+    if (expires_at > now()) {
+      return cached_obo_access_token;
+    }
+
+    oboCache.delete(access_token);
+  }
+
   if (typeof authClient.issuer.metadata.token_endpoint !== 'string') {
     const error = new Error(`OpenID issuer misconfigured. Missing token endpoint.`);
     console.error(error);
@@ -21,7 +34,7 @@ export const getOnBehalfOfAccessToken = async (authClient: Client, access_token:
       assertion: access_token,
     };
 
-    const { access_token: obo_access_token } = await authClient.grant(params, {
+    const { access_token: obo_access_token, expires_at } = await authClient.grant(params, {
       clientAssertionPayload: {
         aud: authClient.issuer.metadata.token_endpoint,
       },
@@ -29,6 +42,10 @@ export const getOnBehalfOfAccessToken = async (authClient: Client, access_token:
 
     if (typeof obo_access_token !== 'string') {
       throw new Error('No on-behalf-of access token from Azure.');
+    }
+
+    if (typeof expires_at === 'number') {
+      oboCache.set(access_token, [obo_access_token, expires_at]);
     }
 
     return obo_access_token;
@@ -45,3 +62,5 @@ export const getOnBehalfOfAccessToken = async (authClient: Client, access_token:
     throw new Error('Unknown error while getting on-behalf-of access token.');
   }
 };
+
+const now = () => Math.round(Date.now() / 1000);
