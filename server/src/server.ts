3941aba7f7df1a0aaaf3b655724706962a81b5ac
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-09-27 09:57:35 +0200

    Add methods and headers to CORS config

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,6 +22,28 @@ server.use(cookieParser());
 server.use(
   cors({
     credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: [
+      'Accept-Language',
+      'Accept',
+      'Cache-Control',
+      'Connection',
+      'Content-Type',
+      'Cookie',
+      'DNT',
+      'Host',
+      'Origin',
+      'Pragma',
+      'Referer',
+      'Sec-Fetch-Dest',
+      'Sec-Fetch-Mode',
+      'Sec-Fetch-Site',
+      'User-Agent',
+      'X-Forwarded-For',
+      'X-Forwarded-Host',
+      'X-Forwarded-Proto',
+      'X-Requested-With',
+    ],
     origin: isDeployedToProd ? applicationDomain : [applicationDomain, /https?:\/\/localhost:\d{4,}/],
   })
 );
