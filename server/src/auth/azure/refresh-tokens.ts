083d76b8bc8cb962a608ed18f3ec287a84d80433
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-10-04 10:37:32 +0200

    Set audience for refresh token request

--- server/src/auth/azure/refresh-tokens.ts
+++ server/src/auth/azure/refresh-tokens.ts
@@ -2,7 +2,11 @@ import { Client } from 'openid-client';
 import { deleteFromRedis, saveSessionData } from '../../redis';
 
 export const refreshAndSaveTokens = async (authClient: Client, session: string, current_refresh_token: string) => {
-  const { access_token, refresh_token } = await authClient.refresh(current_refresh_token);
+  const { access_token, refresh_token } = await authClient.refresh(current_refresh_token, {
+    clientAssertionPayload: {
+      aud: authClient.issuer.metadata.issuer,
+    },
+  });
   if (typeof access_token === 'undefined' || typeof refresh_token === 'undefined') {
     await deleteFromRedis(session);
     throw new Error('Failed to refresh tokens. No error.');
