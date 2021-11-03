e386d6a72d1a453816950cf019ac1cd8ae827b1d
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-11-03 13:59:01 +0100

    Replace Typescript compiler with SWC

--- frontend/webpack.config.local.js
+++ frontend/webpack.config.local.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: 'swc-loader',
         exclude: /node_modules/,
       },
       {
