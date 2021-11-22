aebfa788ad272b866f48602e57dab243397c0e59
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2021-11-22 10:56:42 +0100

    Fix Gosys links for dev and prod (#104)
    Co-authored-by: Daniel Eriksson <daniel.eriksson@nav.no>

--- frontend/src/domain/eksterne-lenker.ts
+++ frontend/src/domain/eksterne-lenker.ts
@@ -1,1 +1,4 @@
-export const EXTERNAL_URL_GOSYS = 'https://gosys.nais.preprod.local/gosys';
+export const EXTERNAL_URL_GOSYS =
+  process.env.NODE_ENV === 'development'
+    ? 'https://gosys.dev.intern.nav.no/gosys'
+    : 'https://gosys.intern.nav.no/gosys';
