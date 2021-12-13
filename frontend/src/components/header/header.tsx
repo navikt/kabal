d2ec0c69856c2653fce3966c286bef1f63b0ae34
Christian Skrøvseth <christian.skrovseth@nav.no>
2021-12-13 11:03:17 +0100

    Add "Mine oppgaver" link to header
    Co-authored-by: Marianne Hval <mahval@users.noreply.github.com>
    Co-authored-by: Daniel Eriksson <eriksson-daniel@users.noreply.github.com>

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -6,13 +6,30 @@ import { User } from './user-menu/user';
 
 export const Header = () => (
   <StyledHeader>
-    <StyledLogo to={'/oppgaver/1'}>
-      <HomeIcon />
-      <StyledLabel>Kabal</StyledLabel>
-    </StyledLogo>
+    <Nav>
+      <StyledLogo to={'/oppgaver/1'}>
+        <HomeIcon />
+        <StyledLabel>Kabal</StyledLabel>
+      </StyledLogo>
+      <Separator />
+      <StyledNavLink to={'/mineoppgaver'}>Mine oppgaver</StyledNavLink>
+    </Nav>
     <User />
   </StyledHeader>
 );
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  font-weight: 600;
+  color: white;
+`;
 
 const StyledHeader = styled.header`
   z-index: 1;
@@ -36,4 +53,11 @@ const StyledLogo = styled(NavLink)`
 
 const StyledLabel = styled.span`
   margin-left: 1em;
+`;
+
+const Separator = styled.div`
+  display: flex;
+  margin: 0 2em;
+  border-left: 1px solid #c9c9c9;
+  height: 100%;
 `;
