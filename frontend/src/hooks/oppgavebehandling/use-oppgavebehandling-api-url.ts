84f52f06b7b4288725e415366a26acd85024b877
Daniel Eriksson <daniel.eriksson@nav.no>
2022-01-05 12:01:43 +0100

    Fix view PDF link

--- frontend/src/hooks/oppgavebehandling/use-oppgavebehandling-api-url.ts
+++ frontend/src/hooks/oppgavebehandling/use-oppgavebehandling-api-url.ts
@@ -1,11 +1,8 @@
 import { OppgaveType } from '../../redux-api/oppgavebehandling-common-types';
 import { useOppgaveType } from '../use-oppgave-type';
 
-export const useOppgavebehandlingApiUrl = (): string => {
-  const type = useOppgaveType();
+const klagebehandlingUrl = '/api/kabal-api/klagebehandlinger/';
+const ankebehandlingUrl = '/api/kabal-anke-api/ankebehandlinger/';
 
-  const klagebehandlingUrl = '/api/kabal-api/klagebehandlinger/';
-  const ankebehandlingUrl = '/api/kabal-anke-api/ankebehandlinger/';
-
-  return type === OppgaveType.ANKEBEHANDLING ? ankebehandlingUrl : klagebehandlingUrl;
-};
+export const useOppgavebehandlingApiUrl = (): string =>
+  useOppgaveType() === OppgaveType.ANKEBEHANDLING ? ankebehandlingUrl : klagebehandlingUrl;
