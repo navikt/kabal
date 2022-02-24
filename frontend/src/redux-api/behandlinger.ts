7e3a6a5e1e119bf69eb89a89a0fb6923d508af34
Christian Skrøvseth <christian.skrovseth@nav.no>
2022-02-24 17:06:42 +0100

    Add /validate to /behandlinger RTK Query API

--- frontend/src/redux-api/behandlinger.ts
+++ frontend/src/redux-api/behandlinger.ts
@@ -1,4 +1,5 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
+import { IApiValidationResponse } from '../functions/error-type-guard';
 import { IVedtakFullfoertResponse } from '../types/oppgavebehandling-response';
 import { KABAL_BEHANDLINGER_BASE_QUERY } from './common';
 import { oppgavebehandlingApi } from './oppgavebehandling';
@@ -7,6 +8,12 @@ export const behandlingerApi = createApi({
   reducerPath: 'behandlingerApi',
   baseQuery: KABAL_BEHANDLINGER_BASE_QUERY,
   endpoints: (builder) => ({
+    validate: builder.query<IApiValidationResponse, string>({
+      query: (oppgaveId) => ({
+        url: `/${oppgaveId}/validate`,
+        validateStatus: ({ status, ok }) => ok || status === 400,
+      }),
+    }),
     finishOppgavebehandling: builder.mutation<IVedtakFullfoertResponse, string>({
       query: (oppgaveId) => ({
         url: `/${oppgaveId}/fullfoer`,
@@ -29,4 +36,4 @@ export const behandlingerApi = createApi({
   }),
 });
 
-export const { useFinishOppgavebehandlingMutation } = behandlingerApi;
+export const { useFinishOppgavebehandlingMutation, useValidateQuery, useLazyValidateQuery } = behandlingerApi;
