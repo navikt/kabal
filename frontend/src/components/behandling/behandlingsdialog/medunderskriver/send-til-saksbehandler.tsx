8843ef7fc1276b2c40f7cae16af400947947e598
Christian Skrøvseth <christian.skrovseth@nav.no>
2022-05-06 15:25:54 +0200

    Fix medunderskriver flow logic
    Co-authored-by: Daniel Eriksson <eriksson-daniel@users.noreply.github.com>

--- frontend/src/components/behandling/behandlingsdialog/medunderskriver/send-til-saksbehandler.tsx
+++ frontend/src/components/behandling/behandlingsdialog/medunderskriver/send-til-saksbehandler.tsx
@@ -4,7 +4,7 @@ import AlertStripe from 'nav-frontend-alertstriper';
 import React from 'react';
 import styled from 'styled-components';
 import { useCanEdit } from '../../../../hooks/use-can-edit';
-import { useCheckIsMedunderskriver } from '../../../../hooks/use-is-medunderskriver';
+import { useIsMedunderskriver } from '../../../../hooks/use-is-medunderskriver';
 import { useSwitchMedunderskriverflytMutation } from '../../../../redux-api/oppgavebehandling';
 import { MedunderskriverFlyt } from '../../../../types/kodeverk';
 import { IOppgavebehandling } from '../../../../types/oppgavebehandling';
@@ -14,7 +14,7 @@ export const SendTilSaksbehandler = ({
   medunderskriverFlyt,
 }: Pick<IOppgavebehandling, 'id' | 'medunderskriverFlyt'>) => {
   const canEdit = useCanEdit();
-  const isMedunderskriver = useCheckIsMedunderskriver();
+  const isMedunderskriver = useIsMedunderskriver();
 
   const [switchMedunderskriverflyt, loader] = useSwitchMedunderskriverflytMutation();
 
@@ -37,7 +37,7 @@ export const SendTilSaksbehandler = ({
           type="button"
           variant="primary"
           size="small"
-          onClick={() => switchMedunderskriverflyt(oppgaveId)}
+          onClick={() => switchMedunderskriverflyt({ oppgaveId, isSaksbehandler: false })}
           disabled={loader.isLoading}
           loading={loader.isLoading}
           data-testid="send-to-saksbehandler"
