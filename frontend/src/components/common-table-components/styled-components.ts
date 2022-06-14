ab9f2b99cb8b9e5a2e5edc99a09ccdbc0d3b5342
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-06-14 12:01:43 +0200

    Fix skewed success status in mine oppgaver and oppgaver

--- frontend/src/components/common-table-components/styled-components.ts
+++ frontend/src/components/common-table-components/styled-components.ts
@@ -2,11 +2,9 @@ import { AlertStripeSuksess } from 'nav-frontend-alertstriper';
 import styled from 'styled-components';
 
 export const SuccessStatus = styled(AlertStripeSuksess)`
-  padding-top: 7px;
-  padding-bottom: 7px;
-  padding-left: 0.5em;
-  padding-right: 0.5em;
-  height: 2.5em;
+  display: flex;
+  align-items: center;
+  height: 48px;
 `;
 
 export const TildelDropdownButton = styled.button`
