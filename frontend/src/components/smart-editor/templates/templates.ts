a9c6806e664404ccac6e0d7594a260d1fbe7ef4d
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-09-09 09:21:42 +0200

    Delete legacy smart editor template, remove unused feature toggling

--- frontend/src/components/smart-editor/templates/templates.ts
+++ frontend/src/components/smart-editor/templates/templates.ts
@@ -3,9 +3,8 @@ import { TemplateTypeEnum } from '../../../types/smart-editor/smart-editor';
 import { ANKEVEDTAK_TEMPLATE } from './ankevedtak';
 import { EMPTY_TEMPLATE } from './empty';
 import { KLAGEVEDTAK_TEMPLATE } from './klagevedtak';
-import { KLAGEVEDTAK_LEGACY_TEMPLATE } from './klagevedtak-legacy';
 
-export const TEMPLATES = [EMPTY_TEMPLATE, KLAGEVEDTAK_LEGACY_TEMPLATE, KLAGEVEDTAK_TEMPLATE, ANKEVEDTAK_TEMPLATE];
+export const TEMPLATES = [EMPTY_TEMPLATE, KLAGEVEDTAK_TEMPLATE, ANKEVEDTAK_TEMPLATE];
 
 export const KLAGE_TEMPLATES = TEMPLATES.filter(
   ({ type }) => type === OppgaveType.KLAGE || type === TemplateTypeEnum.GENERELL
@@ -14,7 +13,5 @@ export const KLAGE_TEMPLATES = TEMPLATES.filter(
 export const ANKE_TEMPLATES = TEMPLATES.filter(
   ({ type }) => type === OppgaveType.ANKE || type === TemplateTypeEnum.GENERELL
 );
-
-export const EXPERIMENTAL_TEMPLATES = [KLAGEVEDTAK_TEMPLATE, ANKEVEDTAK_TEMPLATE];
 
 export const TEMPLATE_IDS = TEMPLATES.map((t) => t.templateId);
