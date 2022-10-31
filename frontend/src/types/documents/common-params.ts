a0b6a3eeafd12e09c199d76c66a83773f0cb7948
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-10-31 14:11:54 +0100

    Show errors for document attachments on archive
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/types/documents/common-params.ts
+++ frontend/src/types/documents/common-params.ts
@@ -1,19 +1,5 @@
-import { Path } from 'slate';
 import { IOppgavebehandlingBaseParams } from '../oppgavebehandling/params';
 
 export interface IDocumentParams extends IOppgavebehandlingBaseParams {
   dokumentId: string;
-}
-
-export enum DocumentValidationErrorType {
-  EMPTY_PLACEHOLDERS = 'EMPTY_PLACEHOLDERS',
-}
-
-interface IDocumentValidationError {
-  type: DocumentValidationErrorType;
-  paths: Path[];
-}
-
-export interface IValidateDocumentResponse {
-  errors: IDocumentValidationError[];
 }
