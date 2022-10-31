384c1d58f76ef2bdcf3422c7ca7d0b2f77116a8c
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-10-31 10:41:00 +0100

    Rename "plassholder" to "innfyllingsfelt"
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/documents/expanded/new/finish-document/views/error-messages.ts
+++ frontend/src/components/documents/expanded/new/finish-document/views/error-messages.ts
@@ -1,5 +1,5 @@
 import { DocumentValidationErrorType } from '../../../../../../types/documents/common-params';
 
 export const ERROR_MESSAGES: Record<DocumentValidationErrorType, string> = {
-  [DocumentValidationErrorType.EMPTY_PLACEHOLDERS]: 'Alle plassholdere må fylles ut',
+  [DocumentValidationErrorType.EMPTY_PLACEHOLDERS]: 'Alle innfyllingsfelt må fylles ut',
 };
