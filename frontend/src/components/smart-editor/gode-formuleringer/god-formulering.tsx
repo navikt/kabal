a5d78ec1b734e64afd8f47dcbdd56597fc9be47c
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-11-01 16:19:25 +0100

    Show expandable preview of gode formuleringer in list
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/smart-editor/gode-formuleringer/god-formulering.tsx
+++ frontend/src/components/smart-editor/gode-formuleringer/god-formulering.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import { Collapse, Expand } from '@navikt/ds-icons';
+import { Button, Heading } from '@navikt/ds-react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Editor } from 'slate';
 import styled from 'styled-components';
 import { IText } from '../../../types/texts/texts';
@@ -14,6 +16,7 @@ interface Props extends IText {
 
 export const GodFormulering = ({ title, content, modified, created, editor, isFocused, onClick }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
     if (isFocused && ref.current !== null) {
@@ -23,8 +26,11 @@ export const GodFormulering = ({ title, content, modified, created, editor, isFo
 
   return (
     <StyledGodFormulering $isFocused={isFocused} ref={ref} onClick={onClick} tabIndex={0}>
-      <Header>
-        <StyledTitle title={title}>{title}</StyledTitle>
+      <Heading title={title} level="1" size="small">
+        {title}
+      </Heading>
+      <ActionWrapper>
+        <DateTime modified={modified} created={created} />
         <AddButton
           editor={editor}
           content={content}
@@ -33,12 +39,45 @@ export const GodFormulering = ({ title, content, modified, created, editor, isFo
         >
           Sett inn
         </AddButton>
-      </Header>
-      <DateTime modified={modified} created={created} />
-      <StyledContent>{content.map((e, i) => renderElement(e, `${i}`))}</StyledContent>
+      </ActionWrapper>
+      <ContentContainer>
+        <StyledContent $isExpanded={isExpanded}>{content.map((e, i) => renderElement(e, `${i}`))}</StyledContent>
+        <ShowMore isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+      </ContentContainer>
     </StyledGodFormulering>
   );
 };
+
+interface ShowMoreProps {
+  isExpanded: boolean;
+  setIsExpanded: (isExpanded: boolean) => void;
+}
+
+const ShowMore = ({ isExpanded, setIsExpanded }: ShowMoreProps) => (
+  <Button
+    onClick={() => setIsExpanded(!isExpanded)}
+    size="xsmall"
+    variant="tertiary"
+    iconPosition="right"
+    icon={isExpanded ? <Collapse aria-hidden /> : <Expand aria-hidden />}
+  >
+    {isExpanded ? 'Vis mindre' : 'Vis mer'}
+  </Button>
+);
+
+const ActionWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: nowrap;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+`;
 
 const StyledGodFormulering = styled.section<{ $isFocused: boolean }>`
   display: flex;
@@ -54,27 +93,25 @@ const StyledGodFormulering = styled.section<{ $isFocused: boolean }>`
   white-space: normal;
 `;
 
-const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 8px;
-  justify-content: space-between;
-`;
-
-const StyledTitle = styled.h1`
-  font-size: 18px;
-  margin: 0;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
-
-const StyledContent = styled.div`
+const StyledContent = styled.div<{ $isExpanded: boolean }>`
   background-color: #fff;
   border-radius: 4px;
   padding: 8px;
-  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.3);
+  padding-bottom: 0;
+  max-height: ${({ $isExpanded }) => ($isExpanded ? 'unset' : '200px')};
+  overflow: hidden;
+  position: relative;
+
+  :after {
+    display: ${({ $isExpanded }) => ($isExpanded ? 'none' : 'block')};
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40px;
+    box-shadow: inset 0 -40px 20px -5px rgb(245, 245, 245, 0.9);
+  }
 
   > :first-child {
     margin-top: 0;
