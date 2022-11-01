c55b48ae1cb7dd86fb8cf484053025f7ba1be992
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-11-01 11:02:27 +0100

    Make smart editor text list sortable
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/smart-editor-texts/smart-editor-texts-list.tsx
+++ frontend/src/components/smart-editor-texts/smart-editor-texts-list.tsx
@@ -1,54 +1,53 @@
 import { FileContent } from '@navikt/ds-icons';
-import { Loader, Search } from '@navikt/ds-react';
-import React, { useMemo, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Loader } from '@navikt/ds-react';
+import React, { useMemo } from 'react';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { stringToRegExp } from '../../functions/string-to-regex';
 import { useGetTextsQuery } from '../../redux-api/texts';
 import { TextTypes } from '../../types/texts/texts';
 import { DateTime } from '../datetime/datetime';
-import { Filters } from './filters';
 import { getPathPrefix } from './functions/get-path-prefix';
 import { useTextQuery } from './hooks/use-text-query';
+import { QueryKey, SortKey, SortOrder, SortableHeader } from './sortable-header';
 import { LimitWarning } from './warning';
 
-interface Props {
+interface TextListProps {
   textType: TextTypes;
-}
-
-export const FilteredTextList = ({ textType }: Props) => {
-  const [filter, setFilter] = useState<string>('');
-
-  const filterRegex = useMemo(() => stringToRegExp(filter), [filter]);
-
-  return (
-    <Container>
-      <Header>
-        <Filters />
-        <Search
-          value={filter}
-          onChange={setFilter}
-          placeholder="Filtrer på tittel"
-          label="Filtrer på tittel"
-          size="small"
-          hideLabel
-        />
-      </Header>
-      <TextList textType={textType} filter={filterRegex} />
-    </Container>
-  );
-};
-
-interface TextListProps extends Props {
   filter: RegExp;
 }
 
-const TextList = ({ textType, filter }: TextListProps) => {
+export const TextList = ({ textType, filter }: TextListProps) => {
   const textQuery = useTextQuery();
-  const { data, isLoading } = useGetTextsQuery(textQuery);
+  const { data = [], isLoading } = useGetTextsQuery(textQuery);
   const query = useParams<{ id: string }>();
+  const [searchParams] = useSearchParams();
 
-  const texts = useMemo(() => data?.filter(({ title }) => filter.test(title)) ?? [], [data, filter]);
+  const sort = useMemo(
+    () => (searchParams.get(QueryKey.SORT) === SortKey.MODIFIED ? SortKey.MODIFIED : SortKey.TITLE),
+    [searchParams]
+  );
+  const order = useMemo(
+    () => (searchParams.get(QueryKey.ORDER) !== SortOrder.DESC ? SortOrder.ASC : SortOrder.DESC),
+    [searchParams]
+  );
+
+  const texts = useMemo(
+    () =>
+      data
+        .filter(({ title }) => filter.test(title))
+        .sort((a, b) => {
+          const isAsc = order === SortOrder.ASC;
+
+          switch (sort) {
+            case SortKey.MODIFIED:
+              return isAsc ? a.modified.localeCompare(b.modified) : b.modified.localeCompare(a.modified);
+            case SortKey.TITLE:
+            default:
+              return isAsc ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+          }
+        }),
+    [order, data, filter, sort]
+  );
 
   if (isLoading || typeof data === 'undefined') {
     return (
@@ -58,52 +57,43 @@ const TextList = ({ textType, filter }: TextListProps) => {
     );
   }
 
-  const prefix = getPathPrefix(textType);
-
   return (
-    <StyledList>
-      {texts.map(({ id, title, modified, created, hjemler, ytelser, utfall, enheter, sections, templates }) => (
-        <ListItem key={id} active={query.id === id}>
-          <StyledLink to={`${prefix}/${id}`}>
-            <StyledTitle>
-              <StyledTitleIcon />
-              <StyledTitleText title={getTitle(title)}>{getTitle(title)}</StyledTitleText>
-            </StyledTitle>
-            <DateTime modified={modified} created={created} />
-            <LimitWarning
-              hjemler={hjemler}
-              ytelser={ytelser}
-              utfall={utfall}
-              enheter={enheter}
-              sections={sections}
-              templates={templates}
-            />
-          </StyledLink>
-        </ListItem>
-      ))}
-    </StyledList>
+    <>
+      <StyledHeaders>
+        <SortableHeader label="Tittel" sortKey={SortKey.TITLE} querySortKey={sort} querySortOrder={order} />
+        <SortableHeader label="Sist endret" sortKey={SortKey.MODIFIED} querySortKey={sort} querySortOrder={order} />
+      </StyledHeaders>
+      <StyledList>
+        {texts.map(({ id, title, modified, created, hjemler, ytelser, utfall, enheter, sections, templates }) => (
+          <ListItem key={id} active={query.id === id}>
+            <StyledLink to={`${getPathPrefix(textType)}/${id}`}>
+              <StyledTitle>
+                <StyledTitleIcon />
+                <StyledTitleText title={getTitle(title)}>{getTitle(title)}</StyledTitleText>
+              </StyledTitle>
+              <DateTime modified={modified} created={created} />
+              <LimitWarning
+                hjemler={hjemler}
+                ytelser={ytelser}
+                utfall={utfall}
+                enheter={enheter}
+                sections={sections}
+                templates={templates}
+              />
+            </StyledLink>
+          </ListItem>
+        ))}
+      </StyledList>
+    </>
   );
 };
 
 const getTitle = (title: string) => (title.trim().length === 0 ? '<Ingen tittel>' : title);
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  height: 100%;
-  overflow-y: auto;
-`;
-
-const Header = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  position: sticky;
-  top: 0;
-  background-color: #fff;
-  box-shadow: 0px 5px 5px -3px rgb(0, 0, 0, 20%);
-  border-radius: 4px;
+const StyledHeaders = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 200px;
+  gap: 0;
 `;
 
 const LoaderOverlay = styled.div`
