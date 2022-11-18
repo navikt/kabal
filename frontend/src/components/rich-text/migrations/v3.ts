a8f0e5ca2bf11e6956d4e9ab812b7599f989ef34
Daniel Eriksson <eriksson.daniel@protonmail.com>
2022-11-18 15:42:47 +0100

    Smart editor text migration: Fix too much recursion error
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/rich-text/migrations/v3.ts
+++ frontend/src/components/rich-text/migrations/v3.ts
@@ -3,7 +3,12 @@ import { PlainText_V3, RichText_Content_V3, RichText_V3, TableCellElementType_V3
 import { PlainText_V4, RichText_Content_V4, RichText_V4 } from '../../../types/rich-text/v4';
 import { isPlainTextType } from '../../../types/texts/texts';
 import { ContentTypeEnum, TextAlignEnum } from '../types/editor-enums';
-import { TableCellElementType, TableElementType } from '../types/editor-types';
+import {
+  TableBodyElementType,
+  TableCellElementType,
+  TableElementType,
+  TableRowElementType,
+} from '../types/editor-types';
 
 export const migrateFromV3ToV4 = (response: RichText_V3 | PlainText_V3): RichText_V4 | PlainText_V4 =>
   isPlainText(response) ? { ...response, version: 4 } : migrateRichTextV3ToV4(response);
@@ -34,7 +39,32 @@ const wrapTableCellContent = (content: RichText_Content_V3): RichText_Content_V4
     return table;
   }
 
-  return wrapTableCellContent(content);
+  if (content.type === 'td') {
+    return migrateTableCell(content);
+  }
+
+  if (content.type === 'tr') {
+    const row: TableRowElementType = {
+      ...content,
+      children: content.children.map(migrateTableCell),
+    };
+
+    return row;
+  }
+
+  if (content.type === 'tbody') {
+    const body: TableBodyElementType = {
+      ...content,
+      children: content.children.map<TableRowElementType>((row) => ({
+        ...row,
+        children: row.children.map(migrateTableCell),
+      })),
+    };
+
+    return body;
+  }
+
+  return content;
 };
 
 const isPlainText = (response: RichText_V3 | PlainText_V3): response is PlainText_V3 =>
