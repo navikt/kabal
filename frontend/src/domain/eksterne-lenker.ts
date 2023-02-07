4bf48862607ba559e4f1e31b14ed9011db64a76f
Daniel Eriksson <>
2023-02-07 09:54:51 +0100

    Add link to Modia
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/domain/eksterne-lenker.ts
+++ frontend/src/domain/eksterne-lenker.ts
@@ -2,3 +2,8 @@ export const EXTERNAL_URL_GOSYS =
   process.env.NODE_ENV === 'development'
     ? 'https://gosys.dev.intern.nav.no/gosys'
     : 'https://gosys.intern.nav.no/gosys';
+
+export const EXTERNAL_URL_MODIA =
+  process.env.NODE_ENV === 'development'
+    ? 'https://app-q1.adeo.no/modiapersonoversikt'
+    : 'https://app.adeo.no/modiapersonoversikt';
