69842899594e701a01d8a5e4d7e76a41cc75bce8
Daniel Eriksson <>
2023-03-07 07:52:40 +0100

    Add new links to Aa-registeret and A-inntekt

--- frontend/src/components/oppgavebehandling-controls/oppgavebehandling-controls.tsx
+++ frontend/src/components/oppgavebehandling-controls/oppgavebehandling-controls.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { EXTERNAL_URL_GOSYS, EXTERNAL_URL_MODIA } from '../../domain/eksterne-lenker';
 import { useOppgave } from '../../hooks/oppgavebehandling/use-oppgave';
-import { ISakspart } from '../../types/oppgavebehandling/oppgavebehandling';
 import { PanelToggles } from '../oppgavebehandling/types';
-import { StyledExtLinkIcon } from '../show-document/styled-components';
-import { ControlPanel, ExternalLink, OppgavebehandlingInformation, OppgavebehandlingTools } from './styled-components';
+import { AaRegisteret, Ainntekt, Gosys, Modia } from './external-links';
+import { ControlPanel, OppgavebehandlingInformation, OppgavebehandlingTools } from './styled-components';
 import { PanelToggleButtons } from './toggle-buttons';
 import { UserInfo } from './user-info';
 
@@ -30,49 +28,11 @@ export const OppgavebehandlingControls = ({ toggles, setPanel, switches }: Oppga
         <PanelToggleButtons togglePanel={setPanel} toggles={toggles} switches={switches} />
       </OppgavebehandlingTools>
       <OppgavebehandlingInformation>
-        <GosysLink sakenGjelder={sakenGjelder} />
-        <ModiaLink sakenGjelder={sakenGjelder} />
+        <Gosys sakenGjelder={sakenGjelder} />
+        <Modia sakenGjelder={sakenGjelder} />
+        <AaRegisteret />
+        <Ainntekt />
       </OppgavebehandlingInformation>
     </ControlPanel>
-  );
-};
-
-interface LinkProps {
-  sakenGjelder: ISakspart;
-}
-
-const GosysLink = ({ sakenGjelder }: LinkProps) => {
-  if (typeof sakenGjelder.person?.foedselsnummer !== 'string') {
-    return null;
-  }
-
-  return (
-    <ExternalLink
-      href={`${EXTERNAL_URL_GOSYS}/personoversikt/fnr=${sakenGjelder.person.foedselsnummer}`}
-      target="_blank"
-      aria-label="Ekstern lenke til Gosys for denne personen"
-      title="Åpne i ny fane"
-      rel="noreferrer"
-    >
-      <span>Gosys</span> <StyledExtLinkIcon title="Ekstern lenke" />
-    </ExternalLink>
-  );
-};
-
-const ModiaLink = ({ sakenGjelder }: LinkProps) => {
-  if (typeof sakenGjelder.person?.foedselsnummer !== 'string') {
-    return null;
-  }
-
-  return (
-    <ExternalLink
-      href={`${EXTERNAL_URL_MODIA}/person/${sakenGjelder.person.foedselsnummer}`}
-      target="_blank"
-      aria-label="Ekstern lenke til Modia for denne personen"
-      title="Åpne i ny fane"
-      rel="noreferrer"
-    >
-      <span>Modia</span> <StyledExtLinkIcon title="Ekstern lenke" />
-    </ExternalLink>
   );
 };
