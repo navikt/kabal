b0e7b27710286ebdc510d18b36728be3e339f9a4
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-04-14 09:56:00 +0200

    Fix missing template texts in smart editor
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/smart-editor/hooks/use-query.ts
+++ frontend/src/components/smart-editor/hooks/use-query.ts
@@ -1,5 +1,6 @@
 import { skipToken } from '@reduxjs/toolkit/dist/query';
 import { useMemo } from 'react';
+import { NONE, NONE_TYPE } from '@app/components/smart-editor-texts/types';
 import { useOppgave } from '@app/hooks/oppgavebehandling/use-oppgave';
 import { useUser } from '@app/simple-api-state/use-user';
 import { NoTemplateIdEnum, TemplateIdEnum } from '@app/types/smart-editor/template-enums';
@@ -11,6 +12,9 @@ interface Params {
   requiredSection?: TemplateSections;
   templateId?: TemplateIdEnum | NoTemplateIdEnum;
 }
+
+const NONE_QUERY: [NONE_TYPE] = [NONE];
+const EMPTY_QUERY: NONE_TYPE[] = [];
 
 export const useQuery = ({ textType, templateId, section, requiredSection }: Params) => {
   const { data, isLoading } = useOppgave();
@@ -24,16 +28,35 @@ export const useQuery = ({ textType, templateId, section, requiredSection }: Par
     const { utfall } = data.resultat;
 
     const query: ApiQuery = {
-      sections: section === undefined ? [] : [section],
-      hjemler: data.resultat.hjemler,
-      ytelser: [data.ytelse],
-      utfall: utfall === null ? [] : [utfall],
-      enheter: [bruker.ansattEnhet.id],
+      sections: getSections(requiredSection, section),
+      hjemler: [...data.resultat.hjemler, NONE],
+      ytelser: [data.ytelse, NONE],
+      utfall: getQuery(utfall), // utfall === null ? NONE_QUERY : [utfall, NONE],
+      enheter: [bruker.ansattEnhet.id, NONE],
+      templates: getQuery(templateId), // templateId === undefined ? NONE_QUERY : [templateId, NONE],
       textType,
-      templates: templateId === undefined ? [] : [templateId],
       requiredSection,
     };
 
     return query;
   }, [bruker, brukerIsLoading, data, isLoading, requiredSection, section, templateId, textType]);
+};
+
+const getSections = (
+  requiredSection: TemplateSections | undefined,
+  section: TemplateSections | undefined
+): (TemplateSections | NONE_TYPE)[] => {
+  if (typeof requiredSection === 'undefined') {
+    return section === undefined ? NONE_QUERY : [section, NONE];
+  }
+
+  return EMPTY_QUERY;
+};
+
+const getQuery = <T>(query: T | null | undefined): (T | NONE_TYPE)[] => {
+  if (query === null || query === undefined) {
+    return NONE_QUERY;
+  }
+
+  return [query, NONE];
 };
