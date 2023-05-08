acdd4c8637ee8c1ad7ac66aa092c49e0e7c9f730
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-05-08 18:37:57 +0200

    RTKQ: Invalidate unfinished oppgave lists after tildeling

--- frontend/src/redux-api/oppgaver/oppgaver.ts
+++ frontend/src/redux-api/oppgaver/oppgaver.ts
@@ -2,6 +2,15 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import { API_BASE_QUERY } from '../common';
 import { ListTagTypes } from '../tag-types';
 
+export enum OppgaveListTagTypes {
+  MINE_UFERDIGE = 'mine-uferdige',
+  MINE_VENTENDE = 'mine-ventende',
+  LEDIGE = 'ledige',
+  ENHETENS_UFERDIGE = 'enhetens-uferdige',
+  ENHETENS_VENTENDE = 'enhetens-ventende',
+  PERSON_AND_OPPGAVER = 'person-and-oppgaver',
+}
+
 export enum DokumenterListTagTypes {
   DOKUMENTER = 'dokumenter',
   TILKNYTTEDEDOKUMENTER = 'tilknyttedeDokumenter',
@@ -18,6 +27,7 @@ export const oppgaverApi = createApi({
     ...Object.values(ListTagTypes),
     ...Object.values(OppgaveTagTypes),
     ...Object.values(DokumenterListTagTypes),
+    ...Object.values(OppgaveListTagTypes),
   ],
   endpoints: () => ({}),
 });
