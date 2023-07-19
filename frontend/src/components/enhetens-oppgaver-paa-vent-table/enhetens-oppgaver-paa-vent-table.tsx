0b0bf5be9bfee3ecebb6531a23ecb3fe9dcc654a
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-07-19 11:07:16 +0200

    Make Enhetens oppgaver på vent filterable, add new columns
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/enhetens-oppgaver-paa-vent-table/enhetens-oppgaver-paa-vent-table.tsx
+++ frontend/src/components/enhetens-oppgaver-paa-vent-table/enhetens-oppgaver-paa-vent-table.tsx
@@ -1,35 +1,40 @@
 import { Heading } from '@navikt/ds-react';
 import { skipToken } from '@reduxjs/toolkit/dist/query/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { OppgaveTable } from '@app/components/common-table-components/oppgave-table/oppgave-table';
 import { ColumnKeyEnum } from '@app/components/common-table-components/types';
 import { OppgaveTableRowsPerPage } from '@app/hooks/settings/use-setting';
 import { useGetEnhetensVentendeOppgaverQuery } from '@app/redux-api/oppgaver/queries/oppgaver';
 import { useUser } from '@app/simple-api-state/use-user';
-import { EnhetensOppgaverParams, SortFieldEnum, SortOrderEnum } from '@app/types/oppgaver';
+import { CommonOppgaverParams, EnhetensOppgaverParams, SortFieldEnum, SortOrderEnum } from '@app/types/oppgaver';
 
 const COLUMNS: ColumnKeyEnum[] = [
   ColumnKeyEnum.Type,
   ColumnKeyEnum.Ytelse,
   ColumnKeyEnum.Hjemmel,
+  ColumnKeyEnum.Age,
+  ColumnKeyEnum.Deadline,
   ColumnKeyEnum.PaaVentTil,
   ColumnKeyEnum.PaaVentReason,
   ColumnKeyEnum.Utfall,
-  ColumnKeyEnum.Tildeling,
+  ColumnKeyEnum.Oppgavestyring,
   ColumnKeyEnum.Open,
 ];
 
 export const EnhetensOppgaverPaaVentTable = () => {
   const { data: bruker } = useUser();
 
+  const [params, setParams] = useState<CommonOppgaverParams>({
+    typer: [],
+    ytelser: [],
+    hjemler: [],
+    tildelteSaksbehandlere: [],
+    rekkefoelge: SortOrderEnum.STIGENDE,
+    sortering: SortFieldEnum.FRIST,
+  });
+
   const queryParams: typeof skipToken | EnhetensOppgaverParams =
-    typeof bruker === 'undefined'
-      ? skipToken
-      : {
-          sortering: SortFieldEnum.FRIST,
-          rekkefoelge: SortOrderEnum.STIGENDE,
-          enhetId: bruker.ansattEnhet.id,
-        };
+    typeof bruker === 'undefined' ? skipToken : { ...params, enhetId: bruker.ansattEnhet.id };
 
   const { data, isError, isFetching, isLoading, refetch } = useGetEnhetensVentendeOppgaverQuery(queryParams, {
     refetchOnFocus: true,
@@ -42,6 +47,8 @@ export const EnhetensOppgaverPaaVentTable = () => {
       <OppgaveTable
         columns={COLUMNS}
         zebraStripes
+        params={params}
+        setParams={setParams}
         data-testid="enhetens-oppgaver-paa-vent-table"
         isLoading={isLoading}
         isFetching={isFetching}
