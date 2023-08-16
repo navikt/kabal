ca9be545a57fc1baa538dd797e846e189dc003fa
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-08-16 11:27:13 +0200

    Hide comment section if unused
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/components/smart-editor/comments/comment-section.tsx
+++ frontend/src/components/smart-editor/comments/comment-section.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { styled } from 'styled-components';
+import { useThreads } from '@app/components/smart-editor/comments/use-threads';
+import { SmartEditorContext } from '@app/components/smart-editor/context';
 import { ThreadList } from './thread-list';
 
-export const CommentSection = () => (
-  <StyledCommentSection>
-    <ThreadList />
-  </StyledCommentSection>
-);
+export const CommentSection = () => {
+  const { attached, orphans, isLoading } = useThreads();
+  const { newCommentSelection } = useContext(SmartEditorContext);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (newCommentSelection === null && attached.length === 0 && orphans.length === 0) {
+    return null;
+  }
+
+  return (
+    <StyledCommentSection>
+      <ThreadList />
+    </StyledCommentSection>
+  );
+};
 
 const StyledCommentSection = styled.div`
   height: 100%;
