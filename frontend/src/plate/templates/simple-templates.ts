3d968bf6c3f924da661ba08de7f28f2c7e568d2f
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-08-28 10:31:03 +0200

    Include signature in generelt brev template

--- frontend/src/plate/templates/simple-templates.ts
+++ frontend/src/plate/templates/simple-templates.ts
@@ -2,13 +2,13 @@ import { deepFreeze } from '@app/functions/deep-freeze';
 import { DistribusjonsType } from '@app/types/documents/documents';
 import { IMutableSmartEditorTemplate, TemplateTypeEnum } from '@app/types/smart-editor/smart-editor';
 import { TemplateIdEnum } from '@app/types/smart-editor/template-enums';
-import { createCurrentDate, createFooter, createHeader, createSimpleParagraph } from './helpers';
+import { createCurrentDate, createFooter, createHeader, createSignature, createSimpleParagraph } from './helpers';
 
 export const GENERELT_BREV_TEMPLATE = deepFreeze<IMutableSmartEditorTemplate>({
   templateId: TemplateIdEnum.GENERELT_BREV,
   type: TemplateTypeEnum.GENERELL,
   tittel: 'Generelt brev',
-  content: [createCurrentDate(), createHeader(), createSimpleParagraph(), createFooter()],
+  content: [createCurrentDate(), createHeader(), createSimpleParagraph(), createSignature(), createFooter()],
   dokumentTypeId: DistribusjonsType.BREV,
 });
 
