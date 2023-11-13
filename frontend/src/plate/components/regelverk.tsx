8e2d89545cf71a2a838ea8d35eb2b6a804a598b4
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-11-13 10:58:22 +0100

    Maltekst / redigerbar maltekst / regelverk: Show buttons on top and as sticky
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- frontend/src/plate/components/regelverk.tsx
+++ frontend/src/plate/components/regelverk.tsx
@@ -1,14 +1,13 @@
 import { GavelSoundBlockIcon } from '@navikt/aksel-icons';
 import { Button, Loader, Tooltip } from '@navikt/ds-react';
 import { skipToken } from '@reduxjs/toolkit/query';
-import { PlateElement, PlateRenderElementProps, findDescendant, replaceNodeChildren } from '@udecode/plate-common';
+import { PlateElement, PlateRenderElementProps, findNodePath, replaceNodeChildren } from '@udecode/plate-common';
 import React, { useCallback, useState } from 'react';
 import { styled } from 'styled-components';
 import { useQuery } from '@app/components/smart-editor/hooks/use-query';
 import { sortWithOrdinals } from '@app/functions/sort-with-ordinals/sort-with-ordinals';
 import { useOppgave } from '@app/hooks/oppgavebehandling/use-oppgave';
 import { SectionContainer, SectionToolbar, SectionTypeEnum } from '@app/plate/components/styled-components';
-import { ELEMENT_REGELVERK_CONTAINER } from '@app/plate/plugins/element-types';
 import { EditorValue, RegelverkContainerElement, RegelverkElement } from '@app/plate/types';
 import { useLazyGetConsumerTextsQuery } from '@app/redux-api/texts/consumer';
 import { RichTextTypes } from '@app/types/common-text-types';
@@ -21,84 +20,20 @@ export const Regelverk = ({
   children,
   element,
   editor,
-}: PlateRenderElementProps<EditorValue, RegelverkElement>) => {
-  const [loading, setLoading] = useState(false);
-  const { data: oppgave } = useOppgave();
-  const query = useQuery({ textType: RichTextTypes.REGELVERK });
-
-  const [getTexts] = useLazyGetConsumerTextsQuery();
-
-  const insertRegelverk = useCallback(async () => {
-    if (oppgave === undefined || query === skipToken) {
-      return;
-    }
-
-    setLoading(true);
-
-    const regelverkEntry = findDescendant<RegelverkElement>(editor, { at: [], match: (n) => n === element });
-
-    if (regelverkEntry === undefined) {
-      return;
-    }
-
-    const [, at] = regelverkEntry;
-
-    const regelverkContainer = findDescendant<RegelverkContainerElement>(editor, {
-      at,
-      match: (n) => n.type === ELEMENT_REGELVERK_CONTAINER,
-    });
-
-    if (regelverkContainer === undefined) {
-      return;
-    }
-
-    const regelverk = (await getTexts(query).unwrap()).filter(isRegelverk);
-
-    const sortedRegelverk = regelverk.sort((a, b) => sortWithOrdinals(a.title, b.title));
-
-    replaceNodeChildren(editor, {
-      at: [...regelverkContainer[1]],
-      nodes: sortedRegelverk.flatMap(({ content }) => content),
-    });
-
-    setLoading(false);
-  }, [editor, element, getTexts, oppgave, query]);
-
-  return (
-    <PlateElement
-      asChild
-      attributes={attributes}
-      element={element}
-      editor={editor}
-      onDragStart={(event) => event.preventDefault()}
-      onDrop={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-      }}
-    >
-      <SectionContainer $sectionType={SectionTypeEnum.REGELVERK} data-element={element.type} aria-disabled={loading}>
-        {children}
-        {loading ? (
-          <LoadingWrapper>
-            <StyledLoader title="Laster..." size="2xlarge" />
-          </LoadingWrapper>
-        ) : null}
-        <SectionToolbar contentEditable={false}>
-          <Tooltip content={loading ? 'Oppdaterer regelverk...' : 'Oppdater regelverk'} delay={0}>
-            <Button
-              icon={<GavelSoundBlockIcon aria-hidden />}
-              onClick={insertRegelverk}
-              variant="tertiary"
-              size="xsmall"
-              contentEditable={false}
-              disabled={loading}
-            />
-          </Tooltip>
-        </SectionToolbar>
-      </SectionContainer>
-    </PlateElement>
-  );
-};
+}: PlateRenderElementProps<EditorValue, RegelverkElement>) => (
+  <PlateElement
+    attributes={attributes}
+    element={element}
+    editor={editor}
+    onDragStart={(event) => event.preventDefault()}
+    onDrop={(e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    }}
+  >
+    {children}
+  </PlateElement>
+);
 
 const LoadingWrapper = styled.div`
   position: absolute;
@@ -123,15 +58,63 @@ export const RegelverkContainer = ({
   children,
   element,
   editor,
-}: PlateRenderElementProps<EditorValue, RegelverkContainerElement>) => (
-  <PlateElement
-    asChild
-    attributes={attributes}
-    element={element}
-    editor={editor}
-    onDragStart={(event) => event.stopPropagation()}
-    onDrop={(e) => e.stopPropagation()}
-  >
-    <div>{children}</div>
-  </PlateElement>
-);
+}: PlateRenderElementProps<EditorValue, RegelverkContainerElement>) => {
+  const [loading, setLoading] = useState(false);
+  const { data: oppgave } = useOppgave();
+  const query = useQuery({ textType: RichTextTypes.REGELVERK });
+
+  const [getTexts] = useLazyGetConsumerTextsQuery();
+
+  const insertRegelverk = useCallback(async () => {
+    if (oppgave === undefined || query === skipToken) {
+      return;
+    }
+
+    setLoading(true);
+
+    const at = findNodePath(editor, element);
+
+    if (at === undefined) {
+      return;
+    }
+
+    const regelverk = (await getTexts(query).unwrap()).filter(isRegelverk);
+    const nodes = regelverk.sort((a, b) => sortWithOrdinals(a.title, b.title)).flatMap(({ content }) => content);
+
+    replaceNodeChildren(editor, { at, nodes });
+
+    setLoading(false);
+  }, [editor, element, getTexts, oppgave, query]);
+
+  return (
+    <PlateElement
+      asChild
+      attributes={attributes}
+      element={element}
+      editor={editor}
+      onDragStart={(event) => event.stopPropagation()}
+      onDrop={(e) => e.stopPropagation()}
+    >
+      <SectionContainer $sectionType={SectionTypeEnum.REGELVERK} data-element={element.type} aria-disabled={loading}>
+        {children}
+        {loading ? (
+          <LoadingWrapper>
+            <StyledLoader title="Laster..." size="2xlarge" />
+          </LoadingWrapper>
+        ) : null}
+        <SectionToolbar contentEditable={false}>
+          <Tooltip content={loading ? 'Oppdaterer regelverk...' : 'Oppdater regelverk'} delay={0}>
+            <Button
+              icon={<GavelSoundBlockIcon aria-hidden />}
+              onClick={insertRegelverk}
+              variant="tertiary"
+              size="xsmall"
+              contentEditable={false}
+              disabled={loading}
+            />
+          </Tooltip>
+        </SectionToolbar>
+      </SectionContainer>
+    </PlateElement>
+  );
+};
