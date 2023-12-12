44a9f3a08ae14a19ddcfcde9ec36b79973d15515
Christian Skrøvseth <christian.skrovseth@gmail.com>
2023-12-12 15:26:31 +0100

    Rename NAV call IDs to traceparent

--- frontend/src/functions/generate-request-id.ts
+++ frontend/src/functions/generate-request-id.ts
@@ -1,1 +1,14 @@
-export const generateRequestId = (): string => crypto.randomUUID().replaceAll('-', '');
+const getUUID = () => crypto.randomUUID().replaceAll('-', '');
+
+const TRACE_VERSION = '00';
+const TRACE_FLAGS = '00';
+
+/** Generates a traceparent ID according to https://www.w3.org/TR/trace-context/#version-format */
+export const generateTraceParent = (): string => {
+  const traceId = getUUID();
+  const parentId = getUUID().substring(0, 16);
+
+  return `${TRACE_VERSION}-${traceId}-${parentId}-${TRACE_FLAGS}`;
+};
+
+export const TRACEPARENT_HEADER = 'traceparent';
