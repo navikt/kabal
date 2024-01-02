4fa2540e633b4cd8a89b676b610c3cf9f7c8c20b
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-01-02 14:01:39 +0100

    Oppgave lists: Shorten flow state labels

--- frontend/src/components/common-table-components/medunderskriver-flow-state-label.tsx
+++ frontend/src/components/common-table-components/medunderskriver-flow-state-label.tsx
@@ -6,6 +6,7 @@ import {
   getTitleLowercase,
 } from '@app/components/behandling/behandlingsdialog/medunderskriver/get-title';
 import { useUser } from '@app/simple-api-state/use-user';
+import { SaksTypeEnum } from '@app/types/kodeverk';
 import { FlowState } from '@app/types/oppgave-common';
 import { IOppgave } from '@app/types/oppgaver';
 
@@ -21,15 +22,27 @@ export const MedudunderskriverFlowStateLabel = ({ medunderskriver, typeId }: Pro
   const isMedunderskriver = medunderskriver.navIdent === data.navIdent;
 
   if (isMedunderskriver && medunderskriver.flowState === FlowState.SENT) {
-    return <StyledTag variant="alt3">{getTitleCapitalized(typeId)}</StyledTag>;
+    return (
+      <StyledTag variant="alt3" title={getTitleCapitalized(typeId)}>
+        {typeId === SaksTypeEnum.ANKE_I_TRYGDERETTEN ? 'Fagansvarlig' : 'MU'}
+      </StyledTag>
+    );
   }
 
   if (!isMedunderskriver && medunderskriver.flowState === FlowState.SENT) {
-    return <StyledTag variant="alt3">Sendt til {getTitleLowercase(typeId)}</StyledTag>;
+    return (
+      <StyledTag variant="alt3" title={`Sendt til ${getTitleLowercase(typeId)}`}>
+        Sendt til {typeId === SaksTypeEnum.ANKE_I_TRYGDERETTEN ? 'fagansvarlig' : 'MU'}
+      </StyledTag>
+    );
   }
 
   if (!isMedunderskriver && medunderskriver.flowState === FlowState.RETURNED) {
-    return <StyledTag variant="info">Sendt tilbake av {getTitleLowercase(typeId)}</StyledTag>;
+    return (
+      <StyledTag variant="info" title={`Tilbake fra ${getTitleLowercase(typeId)}`}>
+        Tilbake fra {typeId === SaksTypeEnum.ANKE_I_TRYGDERETTEN ? 'fagansvarlig' : 'MU'}
+      </StyledTag>
+    );
   }
 
   return null;
