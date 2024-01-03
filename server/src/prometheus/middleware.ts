bad5369102b11c591b4a136160e92fa9629dfa0e
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-01-03 16:00:28 +0100

    Add active clients and session duration histogram metric

--- server/src/prometheus/middleware.ts
+++ server/src/prometheus/middleware.ts
@@ -1,12 +1,6 @@
 import promBundle from 'express-prom-bundle';
-import { NAIS_NAMESPACE } from '@app/config/env';
-import { VERSION } from '@app/config/version';
+import { register } from 'prom-client';
 import { normalizePath } from './normalize-path';
-
-const labels: Record<string, string> = {
-  app_version: VERSION.substring(0, 7),
-  namespace: NAIS_NAMESPACE,
-};
 
 export const metricsMiddleware = promBundle({
   includeMethod: true,
@@ -15,8 +9,7 @@ export const metricsMiddleware = promBundle({
   includeUp: true,
   excludeRoutes: ['/metrics', '/isAlive', '/isReady'],
   normalizePath: ({ originalUrl }) => normalizePath(originalUrl),
-  customLabels: labels,
-  promClient: { collectDefaultMetrics: { labels } },
+  promRegistry: register,
   formatStatusCode: ({ statusCode }) => {
     if (statusCode >= 200 && statusCode < 400) {
       return '2xx (3xx)';
--- server/src/routes/version.ts
+++ server/src/routes/version.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { Gauge, Histogram } from 'prom-client';
 import { VERSION } from '@app/config/config';
+import { registers } from '@app/prometheus/types';
 
 const router = Router();
 
@@ -9,6 +11,15 @@ const HEADERS = {
   'Cache-Control': 'no-cache',
 };
 
+const histogram = new Histogram({
+  name: 'session_duration_seconds',
+  help: 'Duration of user sessions in seconds',
+  buckets: [5, 10, 30, 60, 90, 120, 150, 180, 210, 240, 300, 360, 420, 480, 540, 600, 660, 720].map((n) => n * 60),
+  registers,
+});
+
+const gauge = new Gauge({ name: 'active_clients', help: 'Number of active clients', registers });
+
 export const setupVersionRoute = () => {
   router.get('/version', (req, res) => {
     if (req.headers.accept !== 'text/event-stream') {
@@ -16,9 +27,19 @@ export const setupVersionRoute = () => {
 
       return;
     }
+
+    const stopTimer = histogram.startTimer();
+
+    gauge.inc();
+
     res.writeHead(200, HEADERS);
     res.write('retry: 0\n');
     res.write(`data: ${VERSION}\n\n`);
+
+    res.once('close', () => {
+      stopTimer();
+      gauge.dec();
+    });
   });
 
   return router;
