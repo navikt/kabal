a2ed921ec77f9c9b56a2a6334a8ef0456252b11d
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-01-08 15:08:43 +0100

    Smart editor: automatically convert quotes ("") to gåseøyne («»)

--- frontend/src/plate/plugins/autoformat/rules.ts
+++ frontend/src/plate/plugins/autoformat/rules.ts
@@ -5,12 +5,14 @@ import {
   autoformatMath,
   autoformatPunctuation,
 } from '@udecode/plate-autoformat';
+import { autoformatText } from '@app/plate/plugins/autoformat/text';
 import { EditorAutoformatRule } from '../../types';
 import { autoformatBlocks } from './blocks';
 import { autoformatLists } from './lists';
 import { autoformatMarks } from './marks';
 
 export const autoformatRules: EditorAutoformatRule[] = [
+  ...autoformatText,
   ...autoformatBlocks,
   ...autoformatLists,
   ...autoformatMarks,
