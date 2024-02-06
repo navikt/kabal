bf792cbefa7ff5038064497dcf973d66ecfe47a2
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-02-06 11:12:45 +0100

    Smart editor: Scrub text content

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Scrubber } from 'slate';
 import { App } from './components/app/app';
+
+// https://docs.slatejs.org/api/scrubber
+Scrubber.setScrubber((key, value) => (key === 'text' ? '[FJERNET]' : value));
 
 const container = document.getElementById('app');
 
