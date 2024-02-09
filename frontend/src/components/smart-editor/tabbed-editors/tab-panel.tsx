6da4c47b074098d2b4a4bf6d1e7f2151a03565fc
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-02-09 11:06:26 +0100

    Refactor: Extract tab panel to separate file

---
+++ frontend/src/components/smart-editor/tabbed-editors/tab-panel.tsx
@@ -0,0 +1,85 @@
+import { Tabs } from '@navikt/ds-react';
+import { skipToken } from '@reduxjs/toolkit/query';
+import React, { useEffect, useRef, useState } from 'react';
+import { styled } from 'styled-components';
+import { SmartEditorContextComponent } from '@app/components/smart-editor/context';
+import { Editor } from '@app/components/smart-editor/tabbed-editors/editor';
+import { areDescendantsEqual } from '@app/functions/are-descendants-equal';
+import { useOppgaveId } from '@app/hooks/oppgavebehandling/use-oppgave-id';
+import { useUpdateSmartDocumentMutation } from '@app/redux-api/oppgaver/mutations/smart-document';
+import { ISmartDocument } from '@app/types/documents/documents';
+
+interface TabPanelProps {
+  smartDocument: ISmartDocument;
+}
+
+export const TabPanel = ({ smartDocument }: TabPanelProps) => {
+  const oppgaveId = useOppgaveId();
+  const [update, status] = useUpdateSmartDocumentMutation();
+  const [localContent, setLocalContent] = useState(smartDocument.content);
+  const refContent = useRef(smartDocument.content);
+
+  const { id, content } = smartDocument;
+
+  const smartDocumentRef = useRef<ISmartDocument>(smartDocument);
+
+  // Normal debounce
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (areDescendantsEqual(localContent, content) || oppgaveId === skipToken) {
+        return;
+      }
+
+      update({ content: localContent, oppgaveId, dokumentId: id, version: smartDocument.version });
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, [content, id, oppgaveId, smartDocument.version, update, localContent]);
+
+  // Ensure that smartDocumentRef is always up to date in order to avoid the unmount debounce triggering on archive/delete
+  useEffect(() => {
+    smartDocumentRef.current = smartDocument;
+
+    return () => {
+      smartDocumentRef.current = smartDocument;
+    };
+  }, [smartDocument]);
+
+  // Unmount debounce
+  useEffect(
+    () => () => {
+      if (
+        areDescendantsEqual(refContent.current, smartDocumentRef.current.content) ||
+        oppgaveId === skipToken ||
+        smartDocumentRef.current.isMarkertAvsluttet
+      ) {
+        return;
+      }
+
+      update({ content: refContent.current, oppgaveId, dokumentId: id, version: smartDocumentRef.current.version });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  return (
+    <StyledTabsPanel value={smartDocument.id}>
+      <SmartEditorContextComponent editor={smartDocument}>
+        <Editor
+          key={id}
+          smartDocument={smartDocument}
+          onChange={(c) => {
+            refContent.current = c;
+            setLocalContent(c);
+          }}
+          updateStatus={status}
+        />
+      </SmartEditorContextComponent>
+    </StyledTabsPanel>
+  );
+};
+
+export const StyledTabsPanel = styled(Tabs.Panel)`
+  height: 100%;
+  overflow: hidden;
+`;
