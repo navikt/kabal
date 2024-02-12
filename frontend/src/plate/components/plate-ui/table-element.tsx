3801434869e18d6a4b97cf6ef2f0976d7e18342a
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-02-12 08:47:26 +0100

    Render TableProvider with TableElement (fixes 'Tried to access jotai store 'table' outside of a matching provider.' warnings)

--- frontend/src/plate/components/plate-ui/table-element.tsx
+++ frontend/src/plate/components/plate-ui/table-element.tsx
@@ -1,45 +1,47 @@
-import { PlateElement, PlateElementProps } from '@udecode/plate-common';
-import { useTableElement, useTableElementState } from '@udecode/plate-table';
-import React, { forwardRef } from 'react';
+import { PlateElement, PlateElementProps, withHOC, withRef } from '@udecode/plate-common';
+import { TableProvider, useTableElement, useTableElementState } from '@udecode/plate-table';
+import React from 'react';
 import { styled } from 'styled-components';
 import { ptToEm, pxToEm } from '@app/plate/components/get-scaled-em';
 import { EditorValue, TableElement as ITableElement } from '@app/plate/types';
 
-export const TableElement = forwardRef<
-  React.ElementRef<typeof PlateElement>,
-  PlateElementProps<EditorValue, ITableElement>
->(({ className, children, element, ...props }, ref) => {
-  const { colSizes, minColumnWidth: minWidth } = useTableElementState();
-  const { props: tableProps, colGroupProps } = useTableElement();
+export const TableElement = withHOC(
+  TableProvider,
+  withRef<typeof PlateElement, PlateElementProps<EditorValue, ITableElement>>(
+    ({ className, children, element, ...props }, ref) => {
+      const { colSizes, minColumnWidth: minWidth } = useTableElementState();
+      const { props: tableProps, colGroupProps } = useTableElement();
 
-  return (
-    <PlateElement
-      as={TableStyled}
-      ref={ref}
-      className={className}
-      {...tableProps}
-      {...props}
-      style={{ marginLeft: ptToEm(24 * (element.indent ?? 0)) }}
-      element={element}
-    >
-      <colgroup {...colGroupProps}>
-        {colSizes.map((width, i) =>
-          typeof width !== 'number' ? null : (
-            <col
-              key={i}
-              style={{
-                width: width === 0 ? 'auto' : pxToEm(width),
-                minWidth: typeof minWidth === 'number' ? pxToEm(minWidth) : minWidth,
-              }}
-            />
-          ),
-        )}
-      </colgroup>
+      return (
+        <PlateElement
+          as={TableStyled}
+          ref={ref}
+          className={className}
+          {...tableProps}
+          {...props}
+          style={{ marginLeft: ptToEm(24 * (element.indent ?? 0)) }}
+          element={element}
+        >
+          <colgroup {...colGroupProps}>
+            {colSizes.map((width, i) =>
+              typeof width !== 'number' ? null : (
+                <col
+                  key={i}
+                  style={{
+                    width: width === 0 ? 'auto' : pxToEm(width),
+                    minWidth: typeof minWidth === 'number' ? pxToEm(minWidth) : minWidth,
+                  }}
+                />
+              ),
+            )}
+          </colgroup>
 
-      <tbody style={{ minWidth: '100%' }}>{children}</tbody>
-    </PlateElement>
-  );
-});
+          <tbody style={{ minWidth: '100%' }}>{children}</tbody>
+        </PlateElement>
+      );
+    },
+  ),
+);
 
 TableElement.displayName = 'TableElement';
 
