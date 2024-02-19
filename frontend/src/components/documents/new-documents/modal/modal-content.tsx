345b607742355a9610445516b747150a7a1beab5
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-02-19 16:08:19 +0100

    Fix missing parent options for attachments

--- frontend/src/components/documents/new-documents/modal/modal-content.tsx
+++ frontend/src/components/documents/new-documents/modal/modal-content.tsx
@@ -39,7 +39,7 @@ interface Props {
 export const DocumentModalContent = ({ document, parentDocument, containsRolAttachments }: Props) => {
   const { validationErrors } = useContext(ModalContext);
   const canEditDocument = useCanEditDocument(document, parentDocument);
-  const { pdfOrSmartDocuments, journalfoertDocumentReferences } = useAttachments(document.parentId ?? document.id);
+  const { pdfOrSmartDocuments, journalfoertDocumentReferences } = useAttachments(document.id);
   const canDelete = useCanDeleteDocument(document, containsRolAttachments, parentDocument);
   const [setTitle] = useSetTitleMutation();
   const oppgaveId = useOppgaveId();
