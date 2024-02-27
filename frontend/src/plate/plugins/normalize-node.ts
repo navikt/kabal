63cac256c98030bf3fa627e1f51ac97f9e46598c
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-02-27 13:36:45 +0100

    Log events from empty children normalization

--- frontend/src/plate/plugins/normalize-node.ts
+++ frontend/src/plate/plugins/normalize-node.ts
@@ -1,4 +1,6 @@
-import { TNodeEntry, createPluginFactory, insertNodes, isElement } from '@udecode/plate-common';
+import { TNodeEntry, createPluginFactory, getNode, insertNodes, isElement } from '@udecode/plate-common';
+import { Scrubber } from 'slate';
+import { pushEvent } from '@app/observability';
 
 export const createNormalizeNodePlugin = createPluginFactory({
   key: 'normalize',
@@ -8,6 +10,16 @@ export const createNormalizeNodePlugin = createPluginFactory({
     editor.normalizeNode = ([node, path]: TNodeEntry) => {
       if (isElement(node) && node.children.length === 0) {
         insertNodes(editor, { text: '' }, { at: [...path, 0] });
+
+        const [highestAncestorPath] = path;
+        const highestAncestor =
+          highestAncestorPath === undefined ? undefined : Scrubber.stringify(getNode(editor, [highestAncestorPath]));
+
+        pushEvent('normalized-empty-children', {
+          ancestor: JSON.stringify(highestAncestor),
+          node: JSON.stringify(node),
+          path: JSON.stringify(path),
+        });
 
         return;
       }
