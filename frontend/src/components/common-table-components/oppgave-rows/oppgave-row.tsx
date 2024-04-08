6c4bed689b68caf3f646b0dbba21a6bdc575aeae
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-04-08 09:44:22 +0200

    Add missing type in oppgave tables

--- frontend/src/components/common-table-components/oppgave-rows/oppgave-row.tsx
+++ frontend/src/components/common-table-components/oppgave-rows/oppgave-row.tsx
@@ -60,6 +60,7 @@ const getColumns = (columnKeys: ColumnKeyEnum[], oppgave: IOppgave) =>
   columnKeys.map((key) => {
     switch (key) {
       case ColumnKeyEnum.Type:
+      case ColumnKeyEnum.TypeWithAnkeITrygderetten:
         return (
           <Table.DataCell key={key}>
             <Type type={oppgave.typeId} size="medium" />
