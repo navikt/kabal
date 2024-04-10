5057b7893d8b9f33972ce8cce383ff5fc6ece37e
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-04-10 11:52:34 +0200

    Use correct signature in answers from ROL

--- frontend/src/plate/components/signature/hooks.ts
+++ frontend/src/plate/components/signature/hooks.ts
@@ -1,6 +1,8 @@
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useOppgave } from '@app/hooks/oppgavebehandling/use-oppgave';
 import { useGetSignatureQuery } from '@app/redux-api/bruker';
+import { SaksTypeEnum } from '@app/types/kodeverk';
+import { TemplateIdEnum } from '@app/types/smart-editor/template-enums';
 
 export const useMedunderskriverSignature = () => {
   const { data: oppgave } = useOppgave();
@@ -17,11 +19,27 @@ export const useMedunderskriverSignature = () => {
   return medunderskriverSignature;
 };
 
-export const useSaksbehandlerSignature = () => {
+export const useMainSignature = (template: TemplateIdEnum) => {
   const { data: oppgave } = useOppgave();
+
+  const isRolAnswers = template === TemplateIdEnum.ROL_ANSWERS;
+  const isRolSakstype = oppgave?.typeId === SaksTypeEnum.KLAGE || oppgave?.typeId === SaksTypeEnum.ANKE;
+
   const { data: saksbehandlerSignature } = useGetSignatureQuery(
-    typeof oppgave?.saksbehandler?.navIdent === 'string' ? oppgave.saksbehandler.navIdent : skipToken,
+    !isRolAnswers && typeof oppgave?.saksbehandler?.navIdent === 'string' ? oppgave.saksbehandler.navIdent : skipToken,
   );
+
+  const { data: rolSignature } = useGetSignatureQuery(
+    isRolAnswers && isRolSakstype ? oppgave.rol.employee?.navIdent ?? skipToken : skipToken,
+  );
+
+  if (isRolAnswers) {
+    if (oppgave === undefined || !isRolSakstype || oppgave.rol.employee === null || rolSignature === undefined) {
+      return null;
+    }
+
+    return rolSignature;
+  }
 
   if (oppgave === undefined || oppgave.saksbehandler === null || saksbehandlerSignature === undefined) {
     return null;
