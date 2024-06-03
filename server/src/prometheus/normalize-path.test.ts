4d1b0885065ddb445a7c92edabb6f28ba34dca23
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-06-03 14:32:51 +0200

    Replace jest with vitest

--- server/src/prometheus/normalize-path.test.ts
+++ server/src/prometheus/normalize-path.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it } from 'vitest';
 import { normalizePath } from './normalize-path';
 
 describe('normalizePath', () => {
