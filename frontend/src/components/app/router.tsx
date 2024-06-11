d01042b4eb815f500333a4470018e3963ff0707e
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-06-11 16:49:12 +0200

    Add route protection and no access page

--- frontend/src/components/app/router.tsx
+++ frontend/src/components/app/router.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes as Switch } from 'react-router-dom';
+import { NotFoundPage } from '@app/components/app/not-found-page';
+import { ProtectedRoute } from '@app/components/app/protected-route';
 import { AccessRightsPage } from '@app/pages/access-rights/access-rights';
 import { AdminPage } from '@app/pages/admin/admin';
 import { AnkebehandlingPage } from '@app/pages/ankebehandling/ankebehandling';
@@ -17,62 +19,83 @@ import { SearchPage } from '@app/pages/search/search';
 import { SettingsPage } from '@app/pages/settings/settings';
 import { ToppteksterPage } from '@app/pages/topptekster/topptekster';
 import { TrygderettsankebehandlingPage } from '@app/pages/trygderettsankebehandling/trygderettsankebehandling';
+import { Role } from '@app/types/bruker';
 
 export const Router = () => (
   <Switch>
     <Route path="/" element={<LandingPage />} />
 
-    <Route path="oppgaver" element={<OppgaverPage />} />
+    <Route element={<ProtectedRoute roles={[Role.KABAL_SAKSBEHANDLING, Role.KABAL_ROL]} />}>
+      <Route path="oppgaver" element={<OppgaverPage />} />
+      <Route path="mineoppgaver" element={<MineOppgaverPage />} />
+      <Route path="klagebehandling/:id" element={<KlagebehandlingPage />} />
+      <Route path="ankebehandling/:id" element={<AnkebehandlingPage />} />
+      <Route path="trygderettsankebehandling/:id" element={<TrygderettsankebehandlingPage />} />
+    </Route>
 
-    <Route path="mineoppgaver" element={<MineOppgaverPage />} />
-    <Route path="oppgavestyring" element={<OppgavestyringPage />} />
+    <Route element={<ProtectedRoute roles={[Role.KABAL_INNSYN_EGEN_ENHET, Role.KABAL_KROL]} />}>
+      <Route path="oppgavestyring" element={<OppgavestyringPage />} />
+    </Route>
 
-    <Route path="sok" element={<SearchPage />} />
+    <Route element={<ProtectedRoute roles={[Role.KABAL_SAKSBEHANDLING, Role.KABAL_OPPGAVESTYRING_ALLE_ENHETER]} />}>
+      <Route path="sok" element={<SearchPage />} />
+    </Route>
 
-    <Route path="klagebehandling/:id" element={<KlagebehandlingPage />} />
-    <Route path="ankebehandling/:id" element={<AnkebehandlingPage />} />
-    <Route path="trygderettsankebehandling/:id" element={<TrygderettsankebehandlingPage />} />
+    <Route element={<ProtectedRoute roles={[Role.KABAL_MALTEKSTREDIGERING]} />}>
+      <Route
+        path="maltekstseksjoner/:lang/:id/versjoner/:maltekstseksjonVersionId/tekster/:textId"
+        element={<MaltekstseksjonerPage />}
+      />
+      <Route
+        path="maltekstseksjoner/:lang/:id/versjoner/:maltekstseksjonVersionId"
+        element={<MaltekstseksjonerPage />}
+      />
+      <Route path="maltekstseksjoner/:lang/:id" element={<MaltekstseksjonerPage />} />
+      <Route path="maltekstseksjoner/:lang" element={<MaltekstseksjonerPage />} />
+      <Route path="maltekstseksjoner" element={<MaltekstseksjonerPage />} />
 
-    <Route
-      path="maltekstseksjoner/:lang/:id/versjoner/:maltekstseksjonVersionId/tekster/:textId"
-      element={<MaltekstseksjonerPage />}
-    />
-    <Route path="maltekstseksjoner/:lang/:id/versjoner/:maltekstseksjonVersionId" element={<MaltekstseksjonerPage />} />
-    <Route path="maltekstseksjoner/:lang/:id" element={<MaltekstseksjonerPage />} />
-    <Route path="maltekstseksjoner/:lang" element={<MaltekstseksjonerPage />} />
-    <Route path="maltekstseksjoner" element={<MaltekstseksjonerPage />} />
+      <Route path="maltekster/:lang/:id/versjoner/:versionId" element={<MalteksterPage />} />
+      <Route path="maltekster/:lang/:id" element={<MalteksterPage />} />
+      <Route path="maltekster/:lang" element={<MalteksterPage />} />
+      <Route path="maltekster" element={<MalteksterPage />} />
 
-    <Route path="maltekster/:lang/:id/versjoner/:versionId" element={<MalteksterPage />} />
-    <Route path="maltekster/:lang/:id" element={<MalteksterPage />} />
-    <Route path="maltekster/:lang" element={<MalteksterPage />} />
-    <Route path="maltekster" element={<MalteksterPage />} />
+      <Route path="redigerbare-maltekster/:lang/:id/versjoner/:versionId" element={<RedigerbareMalteksterPage />} />
+      <Route path="redigerbare-maltekster/:lang/:id" element={<RedigerbareMalteksterPage />} />
+      <Route path="redigerbare-maltekster/:lang" element={<RedigerbareMalteksterPage />} />
+      <Route path="redigerbare-maltekster" element={<RedigerbareMalteksterPage />} />
 
-    <Route path="redigerbare-maltekster/:lang/:id/versjoner/:versionId" element={<RedigerbareMalteksterPage />} />
-    <Route path="redigerbare-maltekster/:lang/:id" element={<RedigerbareMalteksterPage />} />
-    <Route path="redigerbare-maltekster/:lang" element={<RedigerbareMalteksterPage />} />
-    <Route path="redigerbare-maltekster" element={<RedigerbareMalteksterPage />} />
+      <Route path="topptekster/:lang/:id/versjoner/:versionId" element={<ToppteksterPage />} />
+      <Route path="topptekster/:lang/:id" element={<ToppteksterPage />} />
+      <Route path="topptekster/:lang" element={<ToppteksterPage />} />
+      <Route path="topptekster" element={<ToppteksterPage />} />
 
-    <Route path="gode-formuleringer/:lang/:id/versjoner/:versionId" element={<GodeFormuleringerPage />} />
-    <Route path="gode-formuleringer/:lang/:id" element={<GodeFormuleringerPage />} />
-    <Route path="gode-formuleringer/:lang" element={<GodeFormuleringerPage />} />
-    <Route path="gode-formuleringer" element={<GodeFormuleringerPage />} />
+      <Route path="bunntekster/:lang/:id/versjoner/:versionId" element={<BunnteksterPage />} />
+      <Route path="bunntekster/:lang/:id" element={<BunnteksterPage />} />
+      <Route path="bunntekster/:lang" element={<BunnteksterPage />} />
+      <Route path="bunntekster" element={<BunnteksterPage />} />
+    </Route>
 
-    <Route path="topptekster/:lang/:id/versjoner/:versionId" element={<ToppteksterPage />} />
-    <Route path="topptekster/:lang/:id" element={<ToppteksterPage />} />
-    <Route path="topptekster/:lang" element={<ToppteksterPage />} />
-    <Route path="topptekster" element={<ToppteksterPage />} />
+    <Route element={<ProtectedRoute roles={[Role.KABAL_FAGTEKSTREDIGERING]} />}>
+      <Route path="gode-formuleringer/:lang/:id/versjoner/:versionId" element={<GodeFormuleringerPage />} />
+      <Route path="gode-formuleringer/:lang/:id" element={<GodeFormuleringerPage />} />
+      <Route path="gode-formuleringer/:lang" element={<GodeFormuleringerPage />} />
+      <Route path="gode-formuleringer" element={<GodeFormuleringerPage />} />
 
-    <Route path="bunntekster/:lang/:id/versjoner/:versionId" element={<BunnteksterPage />} />
-    <Route path="bunntekster/:lang/:id" element={<BunnteksterPage />} />
-    <Route path="bunntekster/:lang" element={<BunnteksterPage />} />
-    <Route path="bunntekster" element={<BunnteksterPage />} />
+      <Route path="regelverk/:id/versjoner/:versionId" element={<RegelverkPage />} />
+      <Route path="regelverk/:id" element={<RegelverkPage />} />
+      <Route path="regelverk" element={<RegelverkPage />} />
+    </Route>
 
-    <Route path="regelverk/:id/versjoner/:versionId" element={<RegelverkPage />} />
-    <Route path="regelverk/:id" element={<RegelverkPage />} />
-    <Route path="regelverk" element={<RegelverkPage />} />
+    <Route element={<ProtectedRoute roles={[Role.KABAL_TILGANGSSTYRING_EGEN_ENHET]} />}>
+      <Route path="tilgangsstyring" element={<AccessRightsPage />} />
+    </Route>
+
+    <Route element={<ProtectedRoute roles={[Role.KABAL_ADMIN]} />}>
+      <Route path="admin" element={<AdminPage />} />
+    </Route>
 
     <Route path="innstillinger" element={<SettingsPage />} />
-    <Route path="tilgangsstyring" element={<AccessRightsPage />} />
-    <Route path="admin" element={<AdminPage />} />
+
+    <Route path="*" element={<NotFoundPage />} />
   </Switch>
 );
