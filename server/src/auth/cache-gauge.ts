959c541f904528c61b8b2a407b831f3f73da09bb
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-07-10 16:15:51 +0200

    Add OBO cache size gauge metric

--- server/src/auth/cache-gauge.ts
+++ server/src/auth/cache-gauge.ts
@@ -1,5 +1,5 @@
 import { proxyRegister } from '@app/prometheus/types';
-import { Counter } from 'prom-client';
+import { Counter, Gauge } from 'prom-client';
 
 const labelNames = ['hit'] as const;
 
@@ -7,5 +7,11 @@ export const cacheGauge = new Counter({
   name: 'obo_cache',
   help: 'Number of requests to the OBO cache. "hit" is the type of hit: "miss", "hit", or "expired".',
   labelNames,
+  registers: [proxyRegister],
+});
+
+export const cacheSizeGauge = new Gauge({
+  name: 'obo_cache_size',
+  help: 'Number of OBO tokens in the cache.',
   registers: [proxyRegister],
 });
