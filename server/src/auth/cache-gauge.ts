7cf503f8a5f6491533b190ae61e785b5f8ef7632
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-07-10 16:17:21 +0200

    Add time histogram for getting OBO tokens

--- server/src/auth/cache-gauge.ts
+++ server/src/auth/cache-gauge.ts
@@ -1,5 +1,5 @@
 import { proxyRegister } from '@app/prometheus/types';
-import { Counter, Gauge } from 'prom-client';
+import { Counter, Gauge, Histogram } from 'prom-client';
 
 const labelNames = ['hit'] as const;
 
@@ -13,5 +13,12 @@ export const cacheGauge = new Counter({
 export const cacheSizeGauge = new Gauge({
   name: 'obo_cache_size',
   help: 'Number of OBO tokens in the cache.',
+  registers: [proxyRegister],
+});
+
+export const oboRequestDuration = new Histogram({
+  name: 'obo_request_duration',
+  help: 'Duration of OBO token requests in milliseconds.',
+  buckets: [0, 10, 100, 200, 300, 400, 500, 600, 800, 900, 1_000],
   registers: [proxyRegister],
 });
