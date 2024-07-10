81fd5d061168d6e3452bf1bd8373918d001c840d
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-07-10 16:14:56 +0200

    Change OBO cache metric type to counter

--- server/src/auth/cache-gauge.ts
+++ server/src/auth/cache-gauge.ts
@@ -1,9 +1,9 @@
 import { proxyRegister } from '@app/prometheus/types';
-import { Gauge } from 'prom-client';
+import { Counter } from 'prom-client';
 
 const labelNames = ['hit'] as const;
 
-export const cacheGauge = new Gauge({
+export const cacheGauge = new Counter({
   name: 'obo_cache',
   help: 'Number of requests to the OBO cache. "hit" is the type of hit: "miss", "hit", or "expired".',
   labelNames,
