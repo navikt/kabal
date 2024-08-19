b41163853947e31edf5ff1edc0c8ff08a4f09a94
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-08-19 14:15:42 +0200

    Remove onLoadDocument

--- server/src/plugins/crdt/collaboration-server.ts
+++ server/src/plugins/crdt/collaboration-server.ts
@@ -5,9 +5,6 @@ import { isDeployed } from '@app/config/env';
 import { getRedisExtension } from '@app/plugins/crdt/redis';
 import { isNotNull } from '@app/functions/guards';
 import { getApiExtension } from '@app/plugins/crdt/api/extension';
-import { slateNodesToInsertDelta } from '@slate-yjs/core';
-import * as Y from 'yjs';
-import { getDocument } from '@app/plugins/crdt/get-document';
 
 const log = getLogger('collaboration');
 
@@ -36,27 +33,6 @@ export const collaborationServer = Server.configure({
       log.error({ msg: 'Tried to close collaboration connection without context' });
       throw new Error('Invalid context');
     }
-  },
-
-  onLoadDocument: async ({ context, document }) => {
-    if (!isConnectionContext(context)) {
-      log.error({ msg: 'Tried to load document without context' });
-      throw new Error('Invalid context');
-    }
-
-    if (document.isEmpty('content')) {
-      log.info({ msg: 'Loading document from API in onLoadDocument' });
-      const res = await getDocument(context.req, context.behandlingId, context.dokumentId);
-      const insertDelta = slateNodesToInsertDelta(res.content);
-      const sharedRoot = document.get('content', Y.XmlText) as unknown as Y.XmlText;
-      sharedRoot.applyDelta(insertDelta);
-    } else {
-      log.info({ msg: 'Document already loaded in onLoadDocument' });
-    }
-
-    log.info({ msg: 'Document loaded in onLoadDocument' });
-
-    return document;
   },
 
   extensions: isDeployed ? [getRedisExtension(), getApiExtension()].filter(isNotNull) : [getApiExtension()],
