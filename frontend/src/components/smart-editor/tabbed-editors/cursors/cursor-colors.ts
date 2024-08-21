50a6d20f286ac539c04796d37f72932a73aea26b
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-08-21 14:43:36 +0200

    Limit cursor colors

--- frontend/src/components/smart-editor/tabbed-editors/cursors/cursor-colors.ts
+++ frontend/src/components/smart-editor/tabbed-editors/cursors/cursor-colors.ts
@@ -11,17 +11,17 @@ const RED: BaseColor = { red: 195, green: 0, blue: 0 };
 // #06893A
 const GREEN: BaseColor = { red: 6, green: 137, blue: 58 };
 // #66CBEC
-const LIGHT_BLUE: BaseColor = { red: 102, green: 203, blue: 236 };
+// const LIGHT_BLUE: BaseColor = { red: 102, green: 203, blue: 236 };
 // #0067C5
 const BLUE: BaseColor = { red: 0, green: 103, blue: 197 };
 // #FF9100
 const ORANGE: BaseColor = { red: 255, green: 145, blue: 0 };
 // #005B82
-const DEEP_BLUE: BaseColor = { red: 0, green: 91, blue: 130 };
+// const DEEP_BLUE: BaseColor = { red: 0, green: 91, blue: 130 };
 // #634689
 const PURPLE: BaseColor = { red: 99, green: 70, blue: 137 };
 
-const COLORS = [RED, GREEN, LIGHT_BLUE, BLUE, ORANGE, DEEP_BLUE, PURPLE];
+const COLORS = [RED, GREEN, BLUE, ORANGE, PURPLE];
 const COLOR_MAX = COLORS.length;
 
 export const getColor = (key: string, opacity: number): string => {
