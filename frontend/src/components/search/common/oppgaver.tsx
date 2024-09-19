570989b2ede10c516f56fd72c196669e86aa1ed3
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-09-19 13:11:42 +0200

    Add saksnr search, remove name search

---
+++ frontend/src/components/search/common/oppgaver.tsx
@@ -0,0 +1,105 @@
+import { Skeleton, Table } from '@navikt/ds-react';
+import { TypedUseQueryHookResult } from '@reduxjs/toolkit/query/react';
+import { styled } from 'styled-components';
+import { ErrorAlert } from '@app/components/search/common/error-alert';
+import { FeilregistrerteOppgaverTable } from '@app/components/search/common/feilregistrerte-oppgaver-table';
+import { FullfoerteOppgaverTable } from '@app/components/search/common/fullfoerte-oppgaver-table';
+import { LedigeOppgaverTable } from '@app/components/search/common/ledige-oppgaver-table';
+import { OppgaverPaaVentTable } from '@app/components/search/common/oppgaver-paa-vent-table';
+import { OppgaverPageWrapper } from '@app/pages/page-wrapper';
+import { staggeredBaseQuery } from '@app/redux-api/common';
+import { IOppgaverResponse } from '@app/types/oppgaver';
+
+// https://github.com/reduxjs/redux-toolkit/issues/1937#issuecomment-1842868277
+// https://redux-toolkit.js.org/rtk-query/usage-with-typescript#typing-query-and-mutation-endpoints
+type OppgaverHookResult = TypedUseQueryHookResult<IOppgaverResponse, string, ReturnType<typeof staggeredBaseQuery>>;
+export type OppgaverQuery = Omit<OppgaverHookResult, 'refetch'> & { refetch: () => void };
+
+export const Oppgaver = ({ data, isFetching, isLoading, error, refetch }: OppgaverQuery) => {
+  if (isLoading) {
+    return <SkeletonTables />;
+  }
+
+  if (error !== undefined) {
+    return (
+      <ErrorContainer>
+        <ErrorAlert error={error} refetch={refetch} isFetching={isFetching}>
+          Feil ved henting av oppgaver
+        </ErrorAlert>
+      </ErrorContainer>
+    );
+  }
+
+  if (data === undefined) {
+    return null;
+  }
+
+  const footerProps = { onRefresh: refetch, isLoading: isFetching };
+
+  return (
+    <OppgaverPageWrapper testId="search-result">
+      <LedigeOppgaverTable oppgaveIds={data.aapneBehandlinger} {...footerProps} />
+      <OppgaverPaaVentTable oppgaveIds={data.paaVentBehandlinger} {...footerProps} />
+      <FullfoerteOppgaverTable oppgaveIds={data.avsluttedeBehandlinger} {...footerProps} />
+      <FeilregistrerteOppgaverTable oppgaveIds={data.feilregistrerteBehandlinger} {...footerProps} />
+    </OppgaverPageWrapper>
+  );
+};
+
+const ErrorContainer = styled.div`
+  display: flex;
+  margin: 16px;
+`;
+
+const SkeletonTables = () => (
+  <Table zebraStripes size="small" style={{ marginLeft: 16 }}>
+    <Table.Body>
+      <SkeletonTable />
+      <SkeletonTable />
+    </Table.Body>
+  </Table>
+);
+
+const SkeletonTable = () => (
+  <Table.Row>
+    <Table.DataCell>
+      <Skeleton height={48} width={70} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={150} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={100} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={75} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={70} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={100} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={90} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={200} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={90} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={120} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={300} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={80} />
+    </Table.DataCell>
+    <Table.DataCell>
+      <Skeleton height={48} width={120} />
+    </Table.DataCell>
+  </Table.Row>
+);
--- frontend/src/components/search/fnr/fnr-search.tsx
+++ frontend/src/components/search/fnr/fnr-search.tsx
@@ -1,93 +1,15 @@
-import { MagnifyingGlassIcon } from '@navikt/aksel-icons';
-import { Alert, Button, Loader } from '@navikt/ds-react';
-import { skipToken } from '@reduxjs/toolkit/query';
-import { styled } from 'styled-components';
-import { formatFoedselsnummer } from '@app/functions/format-id';
-import { useSearchOppgaverByFnrQuery, useSearchPersonByFnrQuery } from '@app/redux-api/oppgaver/queries/oppgaver';
-import { Result } from './result';
-
-const FNR_REGEX = /^\s*\d{6}\s*\d{5}\s*$/;
-const isFnr = (query: string) => FNR_REGEX.test(query);
-const cleanFnr = (fnr: string) => fnr.replaceAll(/\s+/gi, '');
+import { Person, PersonQuery } from '@app/components/search/fnr/person';
+import { Oppgaver, OppgaverQuery } from '../common/oppgaver';
 
 interface Props {
-  queryString: string;
+  personQuery: PersonQuery;
+  oppgaverQuery: OppgaverQuery;
+  fnr: string;
 }
 
-export const FnrSearch = ({ queryString }: Props) => {
-  const query = useCleanQuery(queryString);
-  const {
-    data: oppgaver,
-    isLoading: oppgaverIsLoading,
-    isFetching: oppgaverIsFetching,
-    refetch: refetchOppgaver,
-  } = useSearchOppgaverByFnrQuery(query);
-  const {
-    data: person,
-    isLoading: personIsLoading,
-    isFetching: personIsFetching,
-    refetch: refetchPerson,
-  } = useSearchPersonByFnrQuery(query);
-
-  if (query === skipToken) {
-    return null;
-  }
-
-  if (oppgaverIsLoading || personIsLoading) {
-    return <Loader size="xlarge" />;
-  }
-
-  if (typeof oppgaver === 'undefined') {
-    return (
-      <Alert variant="info" data-testid="search-result-none">
-        <AlertContent>
-          Ingen registrerte oppgaver på denne personen i Kabal.
-          <Button
-            icon={<MagnifyingGlassIcon aria-hidden />}
-            onClick={refetchOppgaver}
-            variant="primary"
-            size="small"
-            loading={oppgaverIsFetching}
-          >
-            Søk på nytt
-          </Button>
-        </AlertContent>
-      </Alert>
-    );
-  }
-
-  if (typeof person === 'undefined') {
-    return (
-      <Alert variant="info" data-testid="search-result-none">
-        <AlertContent>
-          Fant ingen person med ID-nummer {formatFoedselsnummer(query)}.
-          <Button
-            icon={<MagnifyingGlassIcon aria-hidden />}
-            onClick={refetchPerson}
-            variant="primary"
-            size="small"
-            loading={personIsFetching}
-          >
-            Søk på nytt
-          </Button>
-        </AlertContent>
-      </Alert>
-    );
-  }
-
-  return <Result {...oppgaver} person={person} onRefresh={refetchOppgaver} isLoading={oppgaverIsFetching} />;
-};
-
-const useCleanQuery = (queryString: string): string | typeof skipToken => {
-  if (!isFnr(queryString)) {
-    return skipToken;
-  }
-
-  return cleanFnr(queryString);
-};
-
-const AlertContent = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`;
+export const FnrSearch = ({ oppgaverQuery, personQuery, fnr }: Props) => (
+  <>
+    <Person {...personQuery} fnr={fnr} />
+    <Oppgaver {...oppgaverQuery} />
+  </>
+);
