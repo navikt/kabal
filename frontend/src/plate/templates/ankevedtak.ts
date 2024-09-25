06bb5dbd6a90d489f088761b5061049bcb874168
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-09-25 15:55:57 +0200

    Use correct BETO part name

--- frontend/src/plate/templates/ankevedtak.ts
+++ frontend/src/plate/templates/ankevedtak.ts
@@ -1,6 +1,7 @@
 import { ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
 import { deepFreeze } from '@app/functions/deep-freeze';
 import { Source } from '@app/plate/components/label-content';
+import { TemplateSections } from '@app/plate/template-sections';
 import {
   createCurrentDate,
   createFooter,
@@ -10,42 +11,47 @@ import {
   createRegelverk,
   createSignature,
 } from '@app/plate/templates/helpers';
-import { TextAlign } from '@app/plate/types';
+import { EditorValue, TextAlign } from '@app/plate/types';
 import { DistribusjonsType } from '@app/types/documents/documents';
 import { IMutableSmartEditorTemplate } from '@app/types/smart-editor/smart-editor';
 import { TemplateIdEnum } from '@app/types/smart-editor/template-enums';
-import { TemplateSections } from '../template-sections';
+
+const INITIAL_SLATE_VALUE: EditorValue = [
+  createCurrentDate(),
+  createHeader(),
+
+  createMaltekstseksjon(TemplateSections.TITLE),
+
+  {
+    type: ELEMENT_PARAGRAPH,
+    align: TextAlign.LEFT,
+    children: [
+      createLabelContent(Source.KLAGER_IF_EQUAL_TO_SAKEN_GJELDER_NAME, 'Den ankende part'),
+      createLabelContent(Source.SAKEN_GJELDER_IF_DIFFERENT_FROM_KLAGER_NAME, 'Saken gjelder'),
+      createLabelContent(Source.SAKEN_GJELDER_FNR, 'Fødselsnummer'),
+      createLabelContent(Source.KLAGER_IF_DIFFERENT_FROM_SAKEN_GJELDER_NAME, 'Den ankende part'),
+      createLabelContent(Source.SAKSNUMMER, 'Saksnummer'),
+    ],
+  },
+
+  createMaltekstseksjon(TemplateSections.INTRODUCTION),
+  createMaltekstseksjon(TemplateSections.AVGJOERELSE),
+  createMaltekstseksjon(TemplateSections.ANFOERSLER),
+  createMaltekstseksjon(TemplateSections.OPPLYSNINGER),
+  createMaltekstseksjon(TemplateSections.VURDERINGEN),
+  createMaltekstseksjon(TemplateSections.KONKLUSJON),
+  createMaltekstseksjon(TemplateSections.ANKEINFO),
+  createMaltekstseksjon(TemplateSections.SAKSKOSTNADER),
+  createMaltekstseksjon(TemplateSections.GENERELL_INFO),
+
+  createSignature(),
+  createFooter(),
+  createRegelverk(),
+];
 
 export const ANKEVEDTAK_TEMPLATE = deepFreeze<IMutableSmartEditorTemplate>({
   templateId: TemplateIdEnum.ANKEVEDTAK,
   tittel: 'Vedtak/beslutning (anke)',
-  richText: [
-    createCurrentDate(),
-    createHeader(),
-    createMaltekstseksjon(TemplateSections.TITLE),
-    {
-      type: ELEMENT_PARAGRAPH,
-      align: TextAlign.LEFT,
-      children: [
-        createLabelContent(Source.KLAGER_IF_EQUAL_TO_SAKEN_GJELDER_NAME, 'Den ankende part'),
-        createLabelContent(Source.SAKEN_GJELDER_IF_DIFFERENT_FROM_KLAGER_NAME, 'Saken gjelder'),
-        createLabelContent(Source.SAKEN_GJELDER_FNR, 'Fødselsnummer'),
-        createLabelContent(Source.KLAGER_IF_DIFFERENT_FROM_SAKEN_GJELDER_NAME, 'Den ankende part'),
-        createLabelContent(Source.SAKSNUMMER, 'Saksnummer'),
-      ],
-    },
-    createMaltekstseksjon(TemplateSections.INTRODUCTION),
-    createMaltekstseksjon(TemplateSections.AVGJOERELSE),
-    createMaltekstseksjon(TemplateSections.ANFOERSLER),
-    createMaltekstseksjon(TemplateSections.OPPLYSNINGER),
-    createMaltekstseksjon(TemplateSections.VURDERINGEN),
-    createMaltekstseksjon(TemplateSections.KONKLUSJON),
-    createMaltekstseksjon(TemplateSections.ANKEINFO),
-    createMaltekstseksjon(TemplateSections.SAKSKOSTNADER),
-    createMaltekstseksjon(TemplateSections.GENERELL_INFO),
-    createSignature(),
-    createFooter(),
-    createRegelverk(),
-  ],
+  richText: INITIAL_SLATE_VALUE,
   dokumentTypeId: DistribusjonsType.VEDTAKSBREV,
 });
