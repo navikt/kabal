f0a9007fae4269306af7a48eb855a74fa1aac15e
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-10-02 13:22:33 +0200

    Reword innfyllingsfelt error

--- frontend/src/components/documents/new-documents/modal/finish-document/error-messages.ts
+++ frontend/src/components/documents/new-documents/modal/finish-document/error-messages.ts
@@ -1,7 +1,8 @@
 import { DocumentValidationErrorType } from '@app/types/documents/validation';
 
 export const VALIDATION_ERROR_MESSAGES: Record<DocumentValidationErrorType, string> = {
-  [DocumentValidationErrorType.EMPTY_PLACEHOLDER]: 'Alle innfyllingsfelt må fylles ut',
+  [DocumentValidationErrorType.EMPTY_PLACEHOLDER]:
+    'Alle innfyllingsfelt må fylles ut eller fjernes før du kan ferdigstille dokumentet under arbeid. Tomme innfyllingsfelt er markert med rødt i forhåndsvisningen.',
   [DocumentValidationErrorType.EMPTY_REGELVERK]: 'Regelverk må settes inn',
   [DocumentValidationErrorType.WRONG_DATE]: 'Dokumentet har feil dato',
   [DocumentValidationErrorType.DOCUMENT_MODIFIED]: 'Dokumentet har nylig blitt endret',
