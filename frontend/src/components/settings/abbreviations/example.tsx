bbc7b14f631b214d19ede7aef933c30da1407ba7
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-10-11 16:55:49 +0200

    Biome: Improve code for abbreviation examples

--- frontend/src/components/settings/abbreviations/example.tsx
+++ frontend/src/components/settings/abbreviations/example.tsx
@@ -1,10 +1,17 @@
-import { BodyShort, Heading, List, Tag } from '@navikt/ds-react';
+import { format } from '@app/components/settings/abbreviations/format';
+import {
+  type ErrorExampleData,
+  type ExpandingExampleData,
+  ExpansionTypeEnum,
+} from '@app/components/settings/abbreviations/types';
+import { hasOwn } from '@app/functions/object';
+import { BodyShort, Heading, List, Tag, Tooltip } from '@navikt/ds-react';
 import { styled } from 'styled-components';
 
 interface Props {
   title: string;
   children: React.ReactNode;
-  examples: React.ReactNode[];
+  examples: (ExpandingExampleData | ErrorExampleData)[];
   recommended?: boolean;
 }
 
@@ -16,14 +23,25 @@ export const AbbrevationExample = ({ title, children, examples, recommended = fa
         <Tag variant="success" size="xsmall">
           Anbefalt
         </Tag>
-      ) : null}
+      ) : (
+        <Tag variant="warning" size="xsmall">
+          Ikke anbefalt
+        </Tag>
+      )}
     </StyledHeading>
     <BodyShort size="small" spacing>
       {children}
     </BodyShort>
     <List as="ol" size="small">
-      {examples.map((example, index) => (
-        <List.Item key={index}>{example}</List.Item>
+      {examples.map((example) => (
+        <List.Item key={example.short}>
+          <ExpandingExample key={example.short} {...example} />
+          {example.tag === undefined ? null : (
+            <Tag variant="neutral" size="xsmall">
+              {example.tag}
+            </Tag>
+          )}
+        </List.Item>
       ))}
     </List>
   </section>
@@ -35,3 +53,68 @@ const StyledHeading = styled(Heading)`
   align-items: center;
   column-gap: var(--a-spacing-2);
 `;
+
+const ExpandingExample = (data: ExpandingExampleData | ErrorExampleData) => {
+  if (!isExampleData(data)) {
+    return (
+      <>
+        {format(data.short)} blir <ErrorTag />.
+      </>
+    );
+  }
+
+  const { short, long, cap } = data;
+
+  if (cap === ExpansionTypeEnum.AutoCap) {
+    return (
+      <>
+        {format(short)} blir utvidet til {format(long.toLowerCase())}/{format(long)}. <AutoCap />
+      </>
+    );
+  }
+
+  if (cap === ExpansionTypeEnum.AlwaysCap) {
+    return (
+      <>
+        {format(short)} blir utvidet til {format(long)}. <AlwaysCap />
+      </>
+    );
+  }
+
+  if (cap === ExpansionTypeEnum.AlwaysAllCaps) {
+    return (
+      <>
+        {format(short)} blir utvidet til {format(long.toUpperCase())}. <AlwaysAllCaps />
+      </>
+    );
+  }
+};
+
+const isExampleData = (data: ExpandingExampleData | ErrorExampleData): data is ExpandingExampleData =>
+  hasOwn(data, 'cap');
+
+const AutoCap = () => (
+  <Tooltip content="Automatisk stor forbokstav på starten av en setning.">
+    <Tag variant="info" size="xsmall">
+      Automatisk stor forbokstav
+    </Tag>
+  </Tooltip>
+);
+
+const AlwaysCap = () => (
+  <Tag variant="info" size="xsmall">
+    Alltid stor forbokstav
+  </Tag>
+);
+
+const AlwaysAllCaps = () => (
+  <Tag variant="info" size="xsmall">
+    Alltid bare store bokstaver
+  </Tag>
+);
+
+const ErrorTag = () => (
+  <Tag variant="error" size="xsmall">
+    Ikke gjenkjent
+  </Tag>
+);
