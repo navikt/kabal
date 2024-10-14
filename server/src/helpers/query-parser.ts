25b91191283628088ce759b8f9bf54f9a5820534
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-10-14 15:13:04 +0200

    Biome: Server manual fixes

--- server/src/helpers/query-parser.ts
+++ server/src/helpers/query-parser.ts
@@ -3,13 +3,12 @@
  * @param query `foo=bar&baz=abc,123`
  * @returns `{ foo: 'bar', baz: 'abc,123' }`
  */
-export const querystringParser = (query: string): Record<string, string | undefined> =>
-  query.split('&').reduce((acc, q) => {
+export const querystringParser = (query: string): Record<string, string> =>
+  query.split('&').reduce<Record<string, string>>((acc, q) => {
     const [key, value] = q.split('=');
 
-    if (key === undefined || value === undefined) {
-      return acc;
+    if (key !== undefined && value !== undefined) {
+      acc[key] = value;
     }
-
-    return { ...acc, [key]: value };
+    return acc;
   }, {});
