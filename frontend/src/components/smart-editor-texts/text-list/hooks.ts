c32e453a235a1fee5bd10c2c2fe9ca15fedf8b9d
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-11-14 13:49:28 +0100

    Text list: Refactor out (soon-to-be) reusable parts

---
+++ frontend/src/components/smart-editor-texts/text-list/hooks.ts
@@ -0,0 +1,95 @@
+import { type Status, filterByStatus } from '@app/components/smart-editor-texts/status-filter/status-filter';
+import { fuzzySearch } from '@app/components/smart-editor/gode-formuleringer/fuzzy-search';
+import { splitQuery } from '@app/components/smart-editor/gode-formuleringer/split-query';
+import { sortWithOrdinals } from '@app/functions/sort-with-ordinals/sort-with-ordinals';
+import { useRedaktoerLanguage } from '@app/hooks/use-redaktoer-language';
+import type { IMaltekstseksjon } from '@app/types/maltekstseksjoner/responses';
+import { SortOrder } from '@app/types/sort';
+import type { Language } from '@app/types/texts/language';
+import type { IText } from '@app/types/texts/responses';
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { QueryKey, SortKey } from '../sortable-header';
+type RedaktørItem = IText | IMaltekstseksjon;
+type ScoredText<T extends RedaktørItem> = T & {
+  score: number;
+};
+
+export const useFilteredAndSorted = <T extends RedaktørItem>(
+  data: T[],
+  statusFilter: Status[],
+  filter: string,
+  getFilterText: (text: T, language: Language) => string,
+) => {
+  const language = useRedaktoerLanguage();
+  const order = useOrder();
+  const sort = useSort();
+
+  const filteredTexts = useMemo(() => {
+    const filteredByStatus = data.filter((t) => filterByStatus(statusFilter, t));
+
+    if (filter.length === 0) {
+      return filteredByStatus.map((text) => ({ ...text, score: 100 }));
+    }
+
+    const result: ScoredText<T>[] = [];
+
+    for (const text of data) {
+      const filterText = getFilterText(text, language);
+
+      const score = fuzzySearch(splitQuery(filter), filterText);
+
+      if (score > 0) {
+        result.push({ ...text, score });
+      }
+    }
+
+    return result;
+  }, [data, filter, language, statusFilter, getFilterText]);
+
+  const sortedTexts: ScoredText<T>[] = useMemo(
+    () =>
+      filteredTexts.toSorted((a, b) => {
+        const isAsc = order === SortOrder.ASC;
+
+        switch (sort) {
+          case SortKey.SCORE:
+            return isAsc ? a.score - b.score : b.score - a.score;
+          case SortKey.MODIFIED:
+            return isAsc ? a.modified.localeCompare(b.modified) : b.modified.localeCompare(a.modified);
+          default:
+            return isAsc ? sortWithOrdinals(a.title, b.title) : sortWithOrdinals(b.title, a.title);
+        }
+      }),
+    [filteredTexts, order, sort],
+  );
+
+  return sortedTexts;
+};
+
+export const useSort = () => {
+  const [searchParams] = useSearchParams();
+
+  return useMemo(() => {
+    const param = searchParams.get(QueryKey.SORT);
+
+    if (param === SortKey.MODIFIED) {
+      return SortKey.MODIFIED;
+    }
+
+    if (param === SortKey.SCORE) {
+      return SortKey.SCORE;
+    }
+
+    return SortKey.TITLE;
+  }, [searchParams]);
+};
+
+export const useOrder = () => {
+  const [searchParams] = useSearchParams();
+
+  return useMemo(
+    () => (searchParams.get(QueryKey.ORDER) !== SortOrder.DESC ? SortOrder.ASC : SortOrder.DESC),
+    [searchParams],
+  );
+};
--- frontend/src/components/smart-editor-texts/text-list/text-list.tsx
+++ frontend/src/components/smart-editor-texts/text-list/text-list.tsx
@@ -1,9 +1,6 @@
 import { StatusTag } from '@app/components/maltekstseksjoner/status-tag';
-import {
-  StatusFilter,
-  filterByStatus,
-  useStatusFilter,
-} from '@app/components/smart-editor-texts/status-filter/status-filter';
+import { StatusFilter, useStatusFilter } from '@app/components/smart-editor-texts/status-filter/status-filter';
+import { useFilteredAndSorted, useOrder, useSort } from '@app/components/smart-editor-texts/text-list/hooks';
 import {
   Container,
   ListItem,
@@ -15,10 +12,7 @@ import {
   StyledTitleIcon,
   StyledTitleText,
 } from '@app/components/smart-editor-texts/text-list/styled-components';
-import { fuzzySearch } from '@app/components/smart-editor/gode-formuleringer/fuzzy-search';
-import { splitQuery } from '@app/components/smart-editor/gode-formuleringer/split-query';
 import { isGodFormulering, isPlainText, isRegelverk, isRichText } from '@app/functions/is-rich-plain-text';
-import { sortWithOrdinals } from '@app/functions/sort-with-ordinals/sort-with-ordinals';
 import { usePrevious } from '@app/hooks/use-previous';
 import { useGetTextsQuery } from '@app/redux-api/texts/queries';
 import { REGELVERK_TYPE, type TextTypes } from '@app/types/common-text-types';
@@ -27,7 +21,7 @@ import { type Language, UNTRANSLATED } from '@app/types/texts/language';
 import type { IText } from '@app/types/texts/responses';
 import { PercentIcon } from '@navikt/aksel-icons';
 import { Loader } from '@navikt/ds-react';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { getTextAsString } from '../../../plate/functions/get-text-string';
 import { DateTime } from '../../datetime/datetime';
@@ -40,10 +34,6 @@ interface TextListProps {
   filter: string;
   language: Language;
 }
-
-type ScoredText = IText & {
-  score: number;
-};
 
 const getString = (text: IText, language: Language) => {
   if (isRichText(text) || isGodFormulering(text)) {
@@ -73,26 +63,10 @@ export const TextList = ({ textType, filter, language }: TextListProps) => {
   const query = useParams<{ id: string }>();
   const [searchParams, setSearchParams] = useSearchParams();
   const prevFilter = usePrevious(filter);
+
   const [statusFilter] = useStatusFilter();
-
-  const sort = useMemo(() => {
-    const param = searchParams.get(QueryKey.SORT);
-
-    if (param === SortKey.MODIFIED) {
-      return SortKey.MODIFIED;
-    }
-
-    if (param === SortKey.SCORE) {
-      return SortKey.SCORE;
-    }
-
-    return SortKey.TITLE;
-  }, [searchParams]);
-
-  const order = useMemo(
-    () => (searchParams.get(QueryKey.ORDER) !== SortOrder.DESC ? SortOrder.ASC : SortOrder.DESC),
-    [searchParams],
-  );
+  const getFilterText = (text: IText, language: Language) => text.title + (getString(text, language) ?? '');
+  const sortedTexts = useFilteredAndSorted(data, statusFilter, filter, getFilterText);
 
   useEffect(() => {
     if (prevFilter?.length === 0 && filter.length > 0) {
@@ -101,45 +75,6 @@ export const TextList = ({ textType, filter, language }: TextListProps) => {
       setSearchParams(searchParams);
     }
   }, [filter, prevFilter?.length, searchParams, setSearchParams]);
-
-  const filteredTexts = useMemo(() => {
-    const filteredByStatus = data.filter((t) => filterByStatus(statusFilter, t));
-
-    if (filter.length === 0) {
-      return filteredByStatus.map<ScoredText>((text) => ({ ...text, score: 100 }));
-    }
-
-    const result: ScoredText[] = [];
-
-    for (const text of data) {
-      const filterText = text.title + (getString(text, language) ?? '');
-
-      const score = fuzzySearch(splitQuery(filter), filterText);
-
-      if (score > 0) {
-        result.push({ ...text, score });
-      }
-    }
-
-    return result;
-  }, [data, filter, language, statusFilter]);
-
-  const sortedTexts: ScoredText[] = useMemo(
-    () =>
-      filteredTexts.toSorted((a, b) => {
-        const isAsc = order === SortOrder.ASC;
-
-        switch (sort) {
-          case SortKey.SCORE:
-            return isAsc ? a.score - b.score : b.score - a.score;
-          case SortKey.MODIFIED:
-            return isAsc ? a.modified.localeCompare(b.modified) : b.modified.localeCompare(a.modified);
-          default:
-            return isAsc ? sortWithOrdinals(a.title, b.title) : sortWithOrdinals(b.title, a.title);
-        }
-      }),
-    [filteredTexts, order, sort],
-  );
 
   if (isLoading || typeof data === 'undefined') {
     return (
@@ -151,22 +86,7 @@ export const TextList = ({ textType, filter, language }: TextListProps) => {
 
   return (
     <Container>
-      <StyledHeaders>
-        <SortableHeader label="Tittel" sortKey={SortKey.TITLE} querySortKey={sort} querySortOrder={order} />
-        <StatusFilter />
-        <SortableHeader label="Sist endret" sortKey={SortKey.MODIFIED} querySortKey={sort} querySortOrder={order} />
-        <SortableHeader
-          label={
-            <span style={{ display: 'flex', alignContent: 'center' }}>
-              <PercentIcon aria-hidden fontSize={18} />
-            </span>
-          }
-          sortKey={SortKey.SCORE}
-          querySortKey={sort}
-          querySortOrder={order}
-          title="Sorter på søkeresultat"
-        />
-      </StyledHeaders>
+      <Headers />
       <StyledList>
         {sortedTexts.map(({ id, title, modified, publishedDateTime, score, published }) => (
           <ListItem key={id} $active={query.id === id}>
@@ -184,6 +104,30 @@ export const TextList = ({ textType, filter, language }: TextListProps) => {
         ))}
       </StyledList>
     </Container>
+  );
+};
+
+const Headers = () => {
+  const sort = useSort();
+  const order = useOrder();
+
+  return (
+    <StyledHeaders>
+      <SortableHeader label="Tittel" sortKey={SortKey.TITLE} querySortKey={sort} querySortOrder={order} />
+      <StatusFilter />
+      <SortableHeader label="Sist endret" sortKey={SortKey.MODIFIED} querySortKey={sort} querySortOrder={order} />
+      <SortableHeader
+        label={
+          <span style={{ display: 'flex', alignContent: 'center' }}>
+            <PercentIcon aria-hidden fontSize={18} />
+          </span>
+        }
+        sortKey={SortKey.SCORE}
+        querySortKey={sort}
+        querySortOrder={order}
+        title="Sorter på søkeresultat"
+      />
+    </StyledHeaders>
   );
 };
 
