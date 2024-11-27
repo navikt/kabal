15b7c64115323bbf36289ec0cc4650a551f4ff68
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-11-27 10:51:09 +0100

    Improve useMetadataFilters

--- frontend/src/components/smart-editor-texts/hooks/use-metadata-filters.ts
+++ frontend/src/components/smart-editor-texts/hooks/use-metadata-filters.ts
@@ -1,8 +1,8 @@
 import { GOD_FORMULERING_TYPE, PlainTextTypes, REGELVERK_TYPE, type TextTypes } from '@app/types/common-text-types';
 
 export const useMetadataFilters = (textType: TextTypes) => ({
-  templateSection: textType === GOD_FORMULERING_TYPE,
-  ytelseHjemmel: textType === GOD_FORMULERING_TYPE || textType === REGELVERK_TYPE,
-  utfall: textType === GOD_FORMULERING_TYPE || textType === REGELVERK_TYPE,
-  enhet: textType === PlainTextTypes.HEADER || textType === PlainTextTypes.FOOTER,
+  hasTemplateSectionFilter: textType === GOD_FORMULERING_TYPE,
+  hasYtelseHjemmelFilter: textType === GOD_FORMULERING_TYPE || textType === REGELVERK_TYPE,
+  hasUtfallFilter: textType === GOD_FORMULERING_TYPE || textType === REGELVERK_TYPE,
+  hasEnhetFilter: textType === PlainTextTypes.HEADER || textType === PlainTextTypes.FOOTER,
 });
