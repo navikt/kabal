04d86935ce63edac52dd06ab19f2947c07d0cc08
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-11-29 11:03:54 +0100

    Simplify layout

--- frontend/src/components/maltekstseksjoner/maltekstseksjon/texts.tsx
+++ frontend/src/components/maltekstseksjoner/maltekstseksjon/texts.tsx
@@ -3,7 +3,7 @@ import { TextList } from '@app/components/maltekstseksjoner/texts/text-list';
 import type { IGetMaltekstseksjonParams } from '@app/types/common-text-types';
 import type { IMaltekstseksjon } from '@app/types/maltekstseksjoner/responses';
 import { DocPencilIcon, MagnifyingGlassIcon } from '@navikt/aksel-icons';
-import { Tabs } from '@navikt/ds-react';
+import { Tabs, VStack } from '@navikt/ds-react';
 import { useCallback, useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 
@@ -35,28 +35,22 @@ export const MaltekstseksjonTexts = ({ maltekstseksjon, query }: Props) => {
   );
 
   return (
-    <StyledTabs value={activeTab} onChange={onChange}>
-      <Tabs.List>
-        <Tabs.Tab value={TabsEnum.EDIT} label="Redigering" icon={<DocPencilIcon aria-hidden />} />
-        <Tabs.Tab value={TabsEnum.PREVIEW} label="Forhåndsvisning" icon={<MagnifyingGlassIcon aria-hidden />} />
-      </Tabs.List>
-      <StyledTabPanel value={TabsEnum.PREVIEW}>
-        <MaltekstseksjonPreview maltekstseksjon={maltekstseksjon} />
-      </StyledTabPanel>
-      <StyledTabPanel value={TabsEnum.EDIT}>
-        <TextList maltekstseksjon={maltekstseksjon} query={query} />
-      </StyledTabPanel>
-    </StyledTabs>
+    <VStack asChild gridColumn="text-list" overflow="hidden" paddingInline="0 2">
+      <Tabs value={activeTab} onChange={onChange}>
+        <Tabs.List>
+          <Tabs.Tab value={TabsEnum.EDIT} label="Redigering" icon={<DocPencilIcon aria-hidden />} />
+          <Tabs.Tab value={TabsEnum.PREVIEW} label="Forhåndsvisning" icon={<MagnifyingGlassIcon aria-hidden />} />
+        </Tabs.List>
+        <StyledTabPanel value={TabsEnum.PREVIEW}>
+          <MaltekstseksjonPreview maltekstseksjon={maltekstseksjon} />
+        </StyledTabPanel>
+        <StyledTabPanel value={TabsEnum.EDIT}>
+          <TextList maltekstseksjon={maltekstseksjon} query={query} />
+        </StyledTabPanel>
+      </Tabs>
+    </VStack>
   );
 };
-
-const StyledTabs = styled(Tabs)`
-  grid-area: text-list;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  padding-right: var(--a-spacing-2);
-`;
 
 const StyledTabPanel = styled(Tabs.Panel)`
   display: flex;
