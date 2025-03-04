1a17d89aec03c5f6a7705363058225ad5e413732
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-03-04 11:13:00 +0100

    Correctly position table toolbar when editor is zoomed out and toolbar content wraps over multiple rows

--- frontend/src/plate/toolbar/toolbars/table-toolbar.tsx
+++ frontend/src/plate/toolbar/toolbars/table-toolbar.tsx
@@ -1,3 +1,4 @@
+import { ScaleContext } from '@app/plate/status-bar/scale-context';
 import { CommentsButton } from '@app/plate/toolbar/add-comment';
 import { InsertPlaceholder } from '@app/plate/toolbar/insert-placeholder';
 import { Marks } from '@app/plate/toolbar/marks';
@@ -6,7 +7,7 @@ import { TableButtons } from '@app/plate/toolbar/table/table';
 import { type TableElement, useMyPlateEditorRef, useMyPlateEditorState } from '@app/plate/types';
 import { Box, HStack } from '@navikt/ds-react';
 import { BaseTablePlugin } from '@udecode/plate-table';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 interface Props {
   editorId: string;
@@ -65,14 +66,14 @@ export const RedaktoerTableToolbar = (props: Props) => (
   </TableToolbar>
 );
 
+const PADDING = 2;
+const ROW_HEIGHT = 32;
 const TABLE_TOOLBAR_OFFSET = 8;
-const TABLE_TOOLBAR_HEIGHT = 36;
-
-const OFFSET = TABLE_TOOLBAR_OFFSET + TABLE_TOOLBAR_HEIGHT;
 
 const useTablePosition = (table: TableElement | null, container: HTMLDivElement | null) => {
   const editor = useMyPlateEditorRef();
   const [position, setPosition] = useState<[number, number] | null>(null);
+  const { scale } = useContext(ScaleContext);
 
   useEffect(() => {
     if (container === null || table === null) {
@@ -94,7 +95,10 @@ const useTablePosition = (table: TableElement | null, container: HTMLDivElement 
         const tablePos = domNode.getBoundingClientRect();
         const containerPos = container.getBoundingClientRect();
 
-        const top = tablePos.top - containerPos.top - OFFSET;
+        const rows = scaleToNumberOfToolbarRows(scale);
+        const toolbarHeight = ROW_HEIGHT * rows + PADDING * 2;
+
+        const top = tablePos.top - containerPos.top - toolbarHeight - TABLE_TOOLBAR_OFFSET;
         const left = tablePos.left - containerPos.left;
 
         setPosition([top, left]);
@@ -103,7 +107,19 @@ const useTablePosition = (table: TableElement | null, container: HTMLDivElement 
     );
 
     return () => cancelIdleCallback(handle);
-  }, [container, editor, table]);
+  }, [container, editor, table, scale]);
 
   return position;
+};
+
+const scaleToNumberOfToolbarRows = (scale: number) => {
+  if (scale > 76) {
+    return 1;
+  }
+
+  if (scale > 39) {
+    return 2;
+  }
+
+  return 3;
 };
