c511b6ccc40423b3ec4f7dc7f3f5fc250fdc44bb
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-04-01 08:53:01 +0200

    Sett på vent: Let merkantil edit if behandling is tildelt someone + add confirmation for avslutt venteperiode

--- frontend/src/hooks/use-can-edit.ts
+++ frontend/src/hooks/use-can-edit.ts
@@ -3,23 +3,27 @@ import { FlowState } from '@app/types/oppgave-common';
 import { useContext, useMemo } from 'react';
 import { useOppgave } from './oppgavebehandling/use-oppgave';
 
+export const useNoOneCanEdit = () => {
+  const { data, isSuccess } = useOppgave();
+
+  return useMemo(
+    () => !isSuccess || data.isAvsluttetAvSaksbehandler || data.feilregistrering !== null,
+    [data, isSuccess],
+  );
+};
+
 export const useCanEdit = () => {
-  const { data: oppgavebehandling, isLoading: oppgavebehandlingIsLoading } = useOppgave();
+  const { data: oppgavebehandling, isSuccess } = useOppgave();
   const { user } = useContext(StaticDataContext);
+  const noOneCanEdit = useNoOneCanEdit();
 
   return useMemo(() => {
-    if (
-      oppgavebehandlingIsLoading ||
-      oppgavebehandling === undefined ||
-      oppgavebehandling.isAvsluttetAvSaksbehandler ||
-      oppgavebehandling.feilregistrering !== null ||
-      oppgavebehandling.saksbehandler === null
-    ) {
+    if (noOneCanEdit || !isSuccess || oppgavebehandling.saksbehandler === null) {
       return false;
     }
 
     return oppgavebehandling.saksbehandler.navIdent === user.navIdent;
-  }, [oppgavebehandling, oppgavebehandlingIsLoading, user.navIdent]);
+  }, [oppgavebehandling, isSuccess, user.navIdent, noOneCanEdit]);
 };
 
 export const useCanEditBehandling = () => {
