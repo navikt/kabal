3aa500df64f3dc5c409658d9ef512fc6c15735ae
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-04-04 09:30:34 +0200

    Oversendelsesbrev: Make label content / fullmektig the same on both pages

--- frontend/src/plate/templates/oversendelsesbrev.ts
+++ frontend/src/plate/templates/oversendelsesbrev.ts
@@ -57,8 +57,11 @@ export const OVERSENDELSESBREV_TEMPLATE = deepFreeze<IMutableSmartEditorTemplate
       type: BaseParagraphPlugin.key,
       align: TextAlign.LEFT,
       children: [
-        createLabelContent(LabelContentSource.SAKEN_GJELDER_NAME),
+        createLabelContent(LabelContentSource.KLAGER_IF_EQUAL_TO_SAKEN_GJELDER_NAME),
+        createLabelContent(LabelContentSource.SAKEN_GJELDER_IF_DIFFERENT_FROM_KLAGER_NAME),
         createLabelContent(LabelContentSource.SAKEN_GJELDER_FNR),
+        createLabelContent(LabelContentSource.KLAGER_IF_DIFFERENT_FROM_SAKEN_GJELDER_NAME),
+        createFullmektig(),
         createLabelContent(LabelContentSource.SAKSNUMMER),
       ],
     },
