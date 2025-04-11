0a2de58efcd66fa373c14cc253c6004794102098
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-04-11 14:28:20 +0200

    Admin - merkantil tasks: Add status filter and let user finish task with comment

---
+++ frontend/src/components/admin/merkantil-task-list/finish.tsx
@@ -0,0 +1,68 @@
+import { useOnClickOutside } from '@app/hooks/use-on-click-outside';
+import { useCompleteMerkantilTaskMutation } from '@app/redux-api/internal';
+import { CheckmarkIcon } from '@navikt/aksel-icons';
+import { Box, Button, Textarea, VStack } from '@navikt/ds-react';
+import { useRef, useState } from 'react';
+
+interface Props {
+  id: string;
+  dateHandled: string | null;
+}
+
+export const Finish = ({ id, dateHandled }: Props) => {
+  const [complete, { isLoading }] = useCompleteMerkantilTaskMutation();
+  const [isOpen, setIsOpen] = useState(false);
+  const [comment, setComment] = useState('');
+  const ref = useRef<HTMLDivElement>(null);
+  useOnClickOutside(ref, () => setIsOpen(false));
+
+  if (dateHandled !== null) {
+    return null;
+  }
+
+  return (
+    <div className="relative" ref={ref}>
+      <Button onClick={() => setIsOpen(!isOpen)} variant="primary" size="small" icon={<CheckmarkIcon aria-hidden />}>
+        Fullfør med kommentar
+      </Button>
+
+      {isOpen ? (
+        <Box
+          className="right-full z-1"
+          top="0"
+          position="absolute"
+          background="bg-default"
+          width="400px"
+          shadow="medium"
+          borderRadius="medium"
+          padding="4"
+          asChild
+        >
+          <VStack gap="2">
+            <Textarea
+              label="Kommentar"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              size="small"
+              autoFocus
+            />
+            <Button
+              onClick={async () => {
+                await complete({ taskId: id, comment }).unwrap();
+
+                setIsOpen(false);
+                setComment('');
+              }}
+              loading={isLoading}
+              variant="primary"
+              size="small"
+              icon={<CheckmarkIcon aria-hidden />}
+            >
+              Fullfør
+            </Button>
+          </VStack>
+        </Box>
+      ) : null}
+    </div>
+  );
+};
---
+++ frontend/src/components/admin/merkantil-task-list/set-status-filter.tsx
@@ -0,0 +1,32 @@
+import { ToggleGroup } from '@navikt/ds-react';
+
+export enum StatusFilter {
+  ALL = 'ALL',
+  HANDLED = 'HANDLED',
+  UNHANDLED = 'UNHANDLED',
+}
+
+const STATUS_FILTERS = Object.values(StatusFilter);
+
+export const isStatusFilter = (value: string): value is StatusFilter => STATUS_FILTERS.some((f) => f === value);
+
+export const SetStatusFilter = ({
+  filter,
+  setFilter,
+}: { filter: StatusFilter; setFilter: (f: StatusFilter) => void }) => {
+  return (
+    <ToggleGroup
+      value={filter}
+      size="small"
+      onChange={(v) => {
+        if (isStatusFilter(v)) {
+          setFilter(v);
+        }
+      }}
+    >
+      <ToggleGroup.Item value={StatusFilter.ALL}>Alle</ToggleGroup.Item>
+      <ToggleGroup.Item value={StatusFilter.HANDLED}>Håndterte</ToggleGroup.Item>
+      <ToggleGroup.Item value={StatusFilter.UNHANDLED}>Uhåndterte</ToggleGroup.Item>
+    </ToggleGroup>
+  );
+};
