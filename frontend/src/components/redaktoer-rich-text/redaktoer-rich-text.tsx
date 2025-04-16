37ef09ad784543d310a8a19b7a52111be7f4415e
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-04-16 11:09:09 +0200

    Remove forwardRef

--- frontend/src/components/redaktoer-rich-text/redaktoer-rich-text.tsx
+++ frontend/src/components/redaktoer-rich-text/redaktoer-rich-text.tsx
@@ -14,7 +14,7 @@ import type { KabalValue, RichTextEditor } from '@app/plate/types';
 import { ClockDashedIcon } from '@navikt/aksel-icons';
 import { Box, VStack } from '@navikt/ds-react';
 import { Plate, usePlateEditor } from '@udecode/plate-core/react';
-import { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
+import { useContext, useImperativeHandle, useRef } from 'react';
 
 interface Props {
   editorId: string;
@@ -25,56 +25,62 @@ interface Props {
   onFocus?: React.FocusEventHandler<HTMLDivElement>;
   lang: SpellCheckLanguage;
   status?: SavedStatusProps;
+  ref?: React.Ref<RichTextEditor>;
 }
 
-export const RedaktoerRichText = forwardRef<RichTextEditor, Props>(
-  ({ editorId, savedContent, onChange, onKeyDown, readOnly, onFocus, lang, status }, editorRef) => {
-    const ref = useRef<HTMLDivElement>(null);
-    const { user } = useContext(StaticDataContext);
-    const plugins = redaktørPlugins(user);
+export const RedaktoerRichText = ({
+  editorId,
+  savedContent,
+  onChange,
+  onKeyDown,
+  readOnly,
+  onFocus,
+  lang,
+  status,
+  ref: editorRef,
+}: Props) => {
+  const { user } = useContext(StaticDataContext);
+  const ref = useRef<HTMLDivElement>(null);
 
-    const editor = usePlateEditor<KabalValue, (typeof plugins)[0]>({
-      id: editorId,
-      plugins,
-      override: {
-        components: redaktørComponents,
-      },
-      value: savedContent,
-    });
+  const editor = usePlateEditor<KabalValue, ReturnType<typeof redaktørPlugins>[0]>({
+    id: editorId,
+    plugins: redaktørPlugins(user),
+    override: {
+      components: redaktørComponents,
+    },
+    value: savedContent,
+  });
 
-    useImperativeHandle(editorRef, () => editor);
+  useImperativeHandle(editorRef, () => editor);
 
-    return (
-      <ErrorBoundary
-        errorComponent={() => <ErrorComponent textId={editorId} />}
-        actionButton={{
-          onClick: async () => onChange?.({ editor, value: savedContent }),
-          buttonText: 'Gjenopprett tekst',
-          buttonIcon: <ClockDashedIcon aria-hidden />,
-          variant: 'primary',
-          size: 'small',
-        }}
-      >
-        <Plate<RichTextEditor> editor={editor} onValueChange={onChange} readOnly={readOnly}>
-          <VStack asChild padding="4" minWidth="calc(210mm + var(--a-spacing-8))" onKeyDown={onKeyDown}>
-            <Box background="surface-subtle" position="relative" overflowX="hidden" overflowY="auto" flexGrow="1">
-              {readOnly === true ? null : <RedaktoerToolbar />}
+  return (
+    <ErrorBoundary
+      errorComponent={() => <ErrorComponent textId={editorId} />}
+      actionButton={{
+        onClick: async () => onChange?.({ editor, value: savedContent }),
+        buttonText: 'Gjenopprett tekst',
+        buttonIcon: <ClockDashedIcon aria-hidden />,
+        variant: 'primary',
+        size: 'small',
+      }}
+    >
+      <Plate<RichTextEditor> editor={editor} onValueChange={onChange} readOnly={readOnly}>
+        <VStack asChild padding="4" minWidth="calc(210mm + var(--a-spacing-8))" onKeyDown={onKeyDown}>
+          <Box background="surface-subtle" position="relative" overflowX="hidden" overflowY="auto" flexGrow="1">
+            {readOnly === true ? null : <RedaktoerToolbar />}
 
-              <Sheet ref={ref} $minHeight={false}>
-                <FloatingRedaktoerToolbar container={ref.current} editorId={editorId} />
+            <Sheet ref={ref} $minHeight={false}>
+              <FloatingRedaktoerToolbar container={ref.current} editorId={editorId} />
 
-                <RedaktoerTableToolbar container={ref.current} editorId={editorId} />
+              <RedaktoerTableToolbar container={ref.current} editorId={editorId} />
 
-                <KabalPlateEditor id={editorId} readOnly={readOnly} onFocus={onFocus} lang={lang} />
-              </Sheet>
-            </Box>
-          </VStack>
+              <KabalPlateEditor id={editorId} readOnly={readOnly} onFocus={onFocus} lang={lang} />
+            </Sheet>
+          </Box>
+        </VStack>
 
-          <StatusBar>{status === undefined ? null : <SavedStatus {...status} />}</StatusBar>
-        </Plate>
-      </ErrorBoundary>
-    );
-  },
-);
-
-RedaktoerRichText.displayName = 'RedaktoerRichText';
+        <StatusBar>{status === undefined ? null : <SavedStatus {...status} />}</StatusBar>
+      </Plate>
+    </ErrorBoundary>
+  );
+};
