8164e1fbe73341b319e27c45d6bd05a8910913fd
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-04-24 08:21:31 +0200

    Smart editor: Add center align

--- frontend/src/plate/toolbar/align.tsx
+++ frontend/src/plate/toolbar/align.tsx
@@ -2,7 +2,7 @@ import { useIsUnchangeable } from '@app/plate/hooks/use-is-unchangeable';
 import { ToolbarIconButton } from '@app/plate/toolbar/toolbarbutton';
 import { useIsInList } from '@app/plate/toolbar/use-is-in-list';
 import { useMyPlateEditorRef } from '@app/plate/types';
-import { TextAlignLeft, TextAlignRight } from '@styled-icons/fluentui-system-regular';
+import { TextAlignCenter, TextAlignLeft, TextAlignRight } from '@styled-icons/fluentui-system-regular';
 import { setAlign } from '@udecode/plate-alignment';
 import { useAlignDropdownMenuState } from '@udecode/plate-alignment/react';
 import { useIsInHeading } from './use-is-in-heading';
@@ -23,6 +23,13 @@ export const Align = () => {
         onClick={() => setAlign(editor, { value: 'left' })}
         icon={<TextAlignLeft aria-hidden width={24} />}
         active={value === 'left'}
+        disabled={disabled}
+      />
+      <ToolbarIconButton
+        label="Midtstill"
+        onClick={() => setAlign(editor, { value: 'center' })}
+        icon={<TextAlignCenter aria-hidden width={24} />}
+        active={value === 'center'}
         disabled={disabled}
       />
       <ToolbarIconButton
