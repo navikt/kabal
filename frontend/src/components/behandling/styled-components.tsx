7bb22c016bb9aad0cc1caf4f29ae4b5efb448d5b
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-05-08 15:01:55 +0200

    Migrate behandling panel to Tailwind

--- frontend/src/components/behandling/styled-components.tsx
+++ frontend/src/components/behandling/styled-components.tsx
@@ -1,21 +1,11 @@
 import { Box, VStack } from '@navikt/ds-react';
-import { styled } from 'styled-components';
-
-export const StyledContainer = styled.div`
-  display: grid;
-  grid-template-columns: 50% 50%;
-  white-space: normal;
-  width: 750px;
-  grid-column-gap: 1px;
-  flex-grow: 1;
-`;
 
 interface BehandlingSectionProps {
   children: React.ReactNode;
 }
 
 export const StyledBehandlingSection = ({ children }: BehandlingSectionProps) => (
-  <Box padding="4" minHeight="100%">
+  <Box padding="4" minHeight="100%" background="bg-default">
     {children}
   </Box>
 );
