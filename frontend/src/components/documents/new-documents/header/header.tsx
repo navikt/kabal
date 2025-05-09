c0aaa6a665ad3bebc5ab0083f1e275f69033ea32
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-05-09 19:51:46 +0200

    Extract common DropZone component and migrate to Aksel and Tailwind

--- frontend/src/components/documents/new-documents/header/header.tsx
+++ frontend/src/components/documents/new-documents/header/header.tsx
@@ -1,23 +1,64 @@
-import { DeleteDropArea } from '@app/components/documents/new-documents/header/drop-delete-area';
-import { DropZone } from '@app/components/documents/new-documents/header/drop-zone';
-import { listHeaderCSS } from '@app/components/documents/styled-components/list-header';
+import { DragAndDropContext } from '@app/components/documents/drag-context';
+import { DeleteDropArea } from '@app/components/documents/new-documents/header/delete-drop-area';
+import { DropZone } from '@app/components/documents/new-documents/shared/drop-zone';
 import { useIsExpanded } from '@app/components/documents/use-is-expanded';
+import { useOppgaveId } from '@app/hooks/oppgavebehandling/use-oppgave-id';
+import { useHasRole } from '@app/hooks/use-has-role';
+import { useIsSaksbehandler } from '@app/hooks/use-is-saksbehandler';
 import { useValidationError } from '@app/hooks/use-validation-error';
-import { ExclamationmarkTriangleIcon } from '@navikt/aksel-icons';
-import { Heading } from '@navikt/ds-react';
-import { styled } from 'styled-components';
+import { useSetParentMutation } from '@app/redux-api/oppgaver/mutations/documents';
+import { Role } from '@app/types/bruker';
+import { DocumentTypeEnum, type IMainDocument } from '@app/types/documents/documents';
+import { TemplateIdEnum } from '@app/types/smart-editor/template-enums';
+import { ArrowUpIcon, ExclamationmarkTriangleIcon } from '@navikt/aksel-icons';
+import { HStack, Heading } from '@navikt/ds-react';
+import { skipToken } from '@reduxjs/toolkit/query';
+import { useCallback, useContext } from 'react';
 
 interface Props {
   headingId: string;
 }
 
-export const ListHeader = ({ headingId }: Props) => {
+export const NewDocumentsHeader = ({ headingId }: Props) => {
   const errorMessage = useValidationError('underArbeid');
   const [isExpanded] = useIsExpanded();
+  const oppgaveId = useOppgaveId();
+  const [setParent] = useSetParentMutation();
+  const { draggedDocument, clearDragState } = useContext(DragAndDropContext);
+  const isSaksbehandler = useIsSaksbehandler();
+  const hasOppgavestyringRole = useHasRole(Role.KABAL_OPPGAVESTYRING_ALLE_ENHETER);
+  const isDropTarget = (isSaksbehandler || hasOppgavestyringRole) && isDroppable(draggedDocument);
+
+  const onDrop = useCallback(() => {
+    if (isDropTarget && oppgaveId !== skipToken) {
+      setParent({
+        dokumentId: draggedDocument.id,
+        oppgaveId,
+        parentId: null,
+      });
+    }
+
+    clearDragState();
+  }, [clearDragState, draggedDocument, isDropTarget, oppgaveId, setParent]);
 
   return (
-    <NewDocumentsStyledListHeader>
-      <DropZone>
+    <HStack
+      as="header"
+      justify="space-between"
+      align="center"
+      gap="2"
+      paddingBlock="0 2"
+      flexGrow="1"
+      wrap={false}
+      className="border-border-divider border-b"
+    >
+      <DropZone
+        onDrop={onDrop}
+        icon={<ArrowUpIcon aria-hidden />}
+        label="Hoveddokument"
+        active={isDropTarget}
+        className="grow"
+      >
         <Heading size="xsmall" level="2" id={headingId}>
           Dokumenter under arbeid
         </Heading>
@@ -26,14 +67,12 @@ export const ListHeader = ({ headingId }: Props) => {
         ) : null}
       </DropZone>
       {isExpanded ? <DeleteDropArea /> : null}
-    </NewDocumentsStyledListHeader>
+    </HStack>
   );
 };
 
-const NewDocumentsStyledListHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  column-gap: var(--a-spacing-2);
-  ${listHeaderCSS}
-`;
+const isDroppable = (draggedDocument: IMainDocument | null): draggedDocument is IMainDocument =>
+  draggedDocument !== null &&
+  draggedDocument.parentId !== null &&
+  draggedDocument.type !== DocumentTypeEnum.JOURNALFOERT &&
+  draggedDocument.templateId !== TemplateIdEnum.ROL_ANSWERS;
---
+++ frontend/src/components/documents/new-documents/shared/drop-zone.tsx
@@ -0,0 +1,138 @@
+import { merge } from '@app/functions/classes';
+import { HStack } from '@navikt/ds-react';
+import { useCallback, useRef, useState } from 'react';
+
+interface DropZoneProps extends React.HTMLAttributes<HTMLDivElement>, OverlayContentProps {
+  /**
+   * Whether to show the drop overlay or not.
+   */
+  active: boolean;
+  onDrop: (e: React.DragEvent<HTMLDivElement>) => void;
+  danger?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+  overlayClassName?: string;
+}
+
+export const DropZone = ({
+  label,
+  icon,
+  active,
+  className,
+  danger = false,
+  onDrop,
+  children,
+  overlayClassName,
+  ...rest
+}: DropZoneProps) => {
+  const [isDragOver, setIsDragOver] = useState(false);
+  const dragEnterCount = useRef(0);
+
+  const internalOnDrop = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+
+      dragEnterCount.current = 0;
+
+      setIsDragOver(false);
+
+      onDrop(e);
+    },
+    [onDrop],
+  );
+
+  const onDragEnter = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      dragEnterCount.current += 1;
+
+      setIsDragOver(active);
+      rest.onDragEnter?.(e);
+    },
+    [active, rest.onDragEnter],
+  );
+
+  const onDragLeave = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      dragEnterCount.current -= 1;
+
+      if (dragEnterCount.current === 0) {
+        setIsDragOver(false);
+      }
+      rest.onDragLeave?.(e);
+    },
+    [rest.onDragLeave],
+  );
+
+  return (
+    // biome-ignore lint/nursery/noStaticElementInteractions: ¯\_(ツ)_/¯
+    <div
+      data-drop-zone
+      onDrop={internalOnDrop}
+      onDragOver={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+      }}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      className={merge('relative', className)}
+      {...rest}
+    >
+      {children}
+      {active ? (
+        <Overlay label={label} icon={icon} danger={danger} isDragOver={isDragOver} className={overlayClassName} />
+      ) : null}
+    </div>
+  );
+};
+
+interface OverlayContentProps {
+  label?: string;
+  icon?: React.ReactNode;
+}
+
+interface OverlayProps extends OverlayContentProps {
+  danger: boolean;
+  isDragOver: boolean;
+  className?: string;
+}
+
+const Overlay = ({ danger, isDragOver, icon, label, className }: OverlayProps) => {
+  const outlineClassname = danger ? 'outline-border-danger' : 'outline-border-action';
+  const textStrokeClassname = danger ? 'text-stroke-text-on-danger' : 'text-stroke-text-on-action';
+  const backgroundClassname = getBackgroundClassname(isDragOver, danger);
+
+  return (
+    <HStack
+      role="presentation"
+      position="absolute"
+      align="center"
+      justify="center"
+      top="0"
+      left="0"
+      right="0"
+      bottom="0"
+      gap="1"
+      wrap={false}
+      className={merge(
+        `rounded-medium font-bold outline-dashed outline-2 backdrop-blur-[2px] ${textStrokeClassname} ${outlineClassname} ${backgroundClassname}`,
+        className,
+      )}
+    >
+      {icon} <span>{label}</span>
+    </HStack>
+  );
+};
+
+const getBackgroundClassname = (isDragOver: boolean, danger: boolean) => {
+  if (danger) {
+    return isDragOver ? 'bg-drop-background-danger-hover' : 'bg-drop-background-danger';
+  }
+
+  return isDragOver ? 'bg-drop-background-hover' : 'bg-drop-background';
+};
