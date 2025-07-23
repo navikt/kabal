e765e1674aea516e9723f73b1845d991b3cf510b
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-07-23 13:08:02 +0200

    Smart editor table: Enforce static positioning (fixes missing borders in Firefox)

--- frontend/src/plate/components/plate-ui/table-row-element.tsx
+++ frontend/src/plate/components/plate-ui/table-row-element.tsx
@@ -1,7 +1,13 @@
 import { PlateElement, withRef } from 'platejs/react';
 
 export const TableRowElement = withRef<typeof PlateElement>(({ children, ...props }, ref) => (
-  <PlateElement as="tr" ref={ref} {...props} className="static bg-surface-subtle even:bg-surface-default">
+  <PlateElement
+    as="tr"
+    ref={ref}
+    {...props}
+    className="bg-surface-subtle even:bg-surface-default"
+    style={{ position: 'static' }}
+  >
     {children}
   </PlateElement>
 ));
