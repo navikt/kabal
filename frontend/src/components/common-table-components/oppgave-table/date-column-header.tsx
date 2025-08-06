19c71f2042af01230109ab2d92ea745b4bab2625
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-08-06 12:18:43 +0200

    Improve table date header

--- frontend/src/components/common-table-components/oppgave-table/date-column-header.tsx
+++ frontend/src/components/common-table-components/oppgave-table/date-column-header.tsx
@@ -1,41 +1,16 @@
 import { ISO_FORMAT } from '@app/components/date-picker/constants';
 import { DatePickerRange } from '@app/components/date-picker-range/date-picker-range';
 import { type SortFieldEnum, SortOrderEnum } from '@app/types/oppgaver';
-import { ArrowDownIcon, ArrowsUpDownIcon, ArrowUpIcon } from '@navikt/aksel-icons';
-import { Button, HStack, Table, type TableProps } from '@navikt/ds-react';
+import { ArrowsUpDownIcon, SortDownIcon, SortUpIcon } from '@navikt/aksel-icons';
+import { BoxNew, Button, HStack, Table, type TableProps } from '@navikt/ds-react';
 import { format } from 'date-fns';
 import type { DateRange } from 'react-day-picker';
-
-interface SortProps {
-  sortering: SortFieldEnum;
-  rekkefoelge: SortOrderEnum;
-  sortKey: SortFieldEnum;
-  onSortChange: Exclude<TableProps['onSortChange'], undefined>;
-  children: string | null;
-}
 
 interface FilterProps {
   from: string | undefined;
   to: string | undefined;
   setDateRange: (from: string | undefined, to: string | undefined) => void;
 }
-
-const Sort = ({ sortering, rekkefoelge, onSortChange, sortKey, children }: SortProps) => {
-  const sorted = sortering === sortKey;
-  const Icon = getSortIcon(sorted, rekkefoelge);
-
-  return (
-    <Button
-      variant="tertiary"
-      icon={<Icon fontSize={16} />}
-      onClick={() => onSortChange(sortKey)}
-      iconPosition="right"
-      className={`whitespace-nowrap rounded-md px-3 py-4 ${sorted ? 'bg-ax-bg-accent-soft' : 'bg-transparent'}`}
-    >
-      {children}
-    </Button>
-  );
-};
 
 const Filter = ({ from, to, setDateRange }: FilterProps) => {
   const onChange = (range: DateRange | undefined) => {
@@ -67,28 +42,70 @@ export const DateColumnHeader = ({
   ...filterProps
 }: DateColumnHeaderProps) => (
   <Table.ColumnHeader
-    className="whitespace-nowrap"
-    aria-sort={rekkefoelge === SortOrderEnum.ASC ? 'ascending' : 'descending'}
+    className="whitespace-nowrap p-0"
+    aria-sort={sortering === sortKey ? SORT_MAP[rekkefoelge] : 'none'}
   >
-    <HStack align="center" gap="1" wrap={false}>
-      {interactive ? (
-        <>
-          <Sort sortering={sortering} rekkefoelge={rekkefoelge} onSortChange={onSortChange} sortKey={sortKey}>
-            {children}
-          </Sort>
-          <Filter {...filterProps} />
-        </>
-      ) : (
-        children
-      )}
-    </HStack>
+    <BoxNew
+      asChild
+      borderRadius="large"
+      className="-outline-offset-2 hover:outline-2 hover:outline-ax-bg-accent-moderate-hover"
+    >
+      <HStack align="center" gap="1" wrap={false}>
+        {interactive ? (
+          <>
+            <Sort sortering={sortering} rekkefoelge={rekkefoelge} onSortChange={onSortChange} sortKey={sortKey}>
+              {children}
+            </Sort>
+            <Filter {...filterProps} />
+          </>
+        ) : (
+          children
+        )}
+      </HStack>
+    </BoxNew>
   </Table.ColumnHeader>
 );
 
-const getSortIcon = (sorted: boolean, rekkefoelge: SortOrderEnum) => {
-  if (sorted) {
-    return rekkefoelge === SortOrderEnum.ASC ? ArrowUpIcon : ArrowDownIcon;
+interface SortProps {
+  sortering: SortFieldEnum;
+  rekkefoelge: SortOrderEnum;
+  sortKey: SortFieldEnum;
+  onSortChange: Exclude<TableProps['onSortChange'], undefined>;
+  children: string | null;
+}
+
+const Sort = ({ sortering, rekkefoelge, onSortChange, sortKey, children }: SortProps) => (
+  <Button
+    variant="tertiary"
+    size="medium"
+    icon={<SortIcon sortKey={sortKey} sortering={sortering} rekkefoelge={rekkefoelge} />}
+    onClick={() => onSortChange(sortKey)}
+    iconPosition="right"
+    className="whitespace-nowrap"
+  >
+    {children}
+  </Button>
+);
+
+interface SortIconProps {
+  sortKey: SortFieldEnum;
+  sortering: SortFieldEnum;
+  rekkefoelge: SortOrderEnum;
+}
+
+const SortIcon = ({ sortKey, sortering, rekkefoelge }: SortIconProps) => {
+  if (sortering !== sortKey) {
+    return <ArrowsUpDownIcon aria-hidden role="presentation" />;
   }
 
-  return ArrowsUpDownIcon;
+  if (rekkefoelge === SortOrderEnum.ASC) {
+    return <SortUpIcon aria-hidden role="presentation" />;
+  }
+
+  return <SortDownIcon aria-hidden role="presentation" />;
+};
+
+const SORT_MAP: Record<SortOrderEnum, 'ascending' | 'descending'> = {
+  [SortOrderEnum.ASC]: 'ascending',
+  [SortOrderEnum.DESC]: 'descending',
 };
