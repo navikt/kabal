ae6f8df8a9f0324d5d871d205c13681718fe0eda
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-08-06 12:32:20 +0200

    Share SortButton and clarify UI

--- frontend/src/components/common-table-components/oppgave-table/date-column-header.tsx
+++ frontend/src/components/common-table-components/oppgave-table/date-column-header.tsx
@@ -1,8 +1,11 @@
+import {
+  getAriaSort,
+  SortButton,
+  type SortButtonProps,
+} from '@app/components/common-table-components/oppgave-table/sort-header';
 import { ISO_FORMAT } from '@app/components/date-picker/constants';
 import { DatePickerRange } from '@app/components/date-picker-range/date-picker-range';
-import { type SortFieldEnum, SortOrderEnum } from '@app/types/oppgaver';
-import { ArrowsUpDownIcon, SortDownIcon, SortUpIcon } from '@navikt/aksel-icons';
-import { BoxNew, Button, HStack, Table, type TableProps } from '@navikt/ds-react';
+import { BoxNew, HStack, Table } from '@navikt/ds-react';
 import { format } from 'date-fns';
 import type { DateRange } from 'react-day-picker';
 
@@ -25,10 +28,10 @@ const Filter = ({ from, to, setDateRange }: FilterProps) => {
     );
   };
 
-  return <DatePickerRange onChange={onChange} selected={{ from, to }} />;
+  return <DatePickerRange onChange={onChange} selected={{ from, to }} buttonClassName="rounded-l-none" />;
 };
 
-interface DateColumnHeaderProps extends FilterProps, SortProps {
+interface DateColumnHeaderProps extends FilterProps, SortButtonProps {
   interactive?: boolean;
 }
 
@@ -41,21 +44,26 @@ export const DateColumnHeader = ({
   interactive = true,
   ...filterProps
 }: DateColumnHeaderProps) => (
-  <Table.ColumnHeader
-    className="whitespace-nowrap p-0"
-    aria-sort={sortering === sortKey ? SORT_MAP[rekkefoelge] : 'none'}
-  >
+  <Table.ColumnHeader className="whitespace-nowrap p-0" aria-sort={getAriaSort(sortKey, sortering, rekkefoelge)}>
     <BoxNew
       asChild
       borderRadius="large"
+      width="fit-content"
       className="-outline-offset-2 hover:outline-2 hover:outline-ax-bg-accent-moderate-hover"
     >
-      <HStack align="center" gap="1" wrap={false}>
+      <HStack align="stretch" gap="1" wrap={false}>
         {interactive ? (
           <>
-            <Sort sortering={sortering} rekkefoelge={rekkefoelge} onSortChange={onSortChange} sortKey={sortKey}>
+            <SortButton
+              sortering={sortering}
+              rekkefoelge={rekkefoelge}
+              onSortChange={onSortChange}
+              sortKey={sortKey}
+              className="rounded-r-none"
+            >
               {children}
-            </Sort>
+            </SortButton>
+
             <Filter {...filterProps} />
           </>
         ) : (
@@ -65,47 +73,3 @@ export const DateColumnHeader = ({
     </BoxNew>
   </Table.ColumnHeader>
 );
-
-interface SortProps {
-  sortering: SortFieldEnum;
-  rekkefoelge: SortOrderEnum;
-  sortKey: SortFieldEnum;
-  onSortChange: Exclude<TableProps['onSortChange'], undefined>;
-  children: string | null;
-}
-
-const Sort = ({ sortering, rekkefoelge, onSortChange, sortKey, children }: SortProps) => (
-  <Button
-    variant="tertiary"
-    size="medium"
-    icon={<SortIcon sortKey={sortKey} sortering={sortering} rekkefoelge={rekkefoelge} />}
-    onClick={() => onSortChange(sortKey)}
-    iconPosition="right"
-    className="whitespace-nowrap"
-  >
-    {children}
-  </Button>
-);
-
-interface SortIconProps {
-  sortKey: SortFieldEnum;
-  sortering: SortFieldEnum;
-  rekkefoelge: SortOrderEnum;
-}
-
-const SortIcon = ({ sortKey, sortering, rekkefoelge }: SortIconProps) => {
-  if (sortering !== sortKey) {
-    return <ArrowsUpDownIcon aria-hidden role="presentation" />;
-  }
-
-  if (rekkefoelge === SortOrderEnum.ASC) {
-    return <SortUpIcon aria-hidden role="presentation" />;
-  }
-
-  return <SortDownIcon aria-hidden role="presentation" />;
-};
-
-const SORT_MAP: Record<SortOrderEnum, 'ascending' | 'descending'> = {
-  [SortOrderEnum.ASC]: 'ascending',
-  [SortOrderEnum.DESC]: 'descending',
-};
