9c7efaac863f4f285bdea9ec36bec2583e292500
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-08-06 14:26:14 +0200

    Fix table toolbar icons color

--- frontend/src/plate/toolbar/table/icons/add-column-left.tsx
+++ frontend/src/plate/toolbar/table/icons/add-column-left.tsx
@@ -1,5 +1,12 @@
 export const AddColumnLeftIcon = () => (
-  <svg enableBackground="new 0 0 20 20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+  <svg
+    enableBackground="new 0 0 20 20"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+  >
     <title>Legg til kolonne til venstre</title>
     <path
       clipRule="evenodd"
--- frontend/src/plate/toolbar/table/icons/add-row-below.tsx
+++ frontend/src/plate/toolbar/table/icons/add-row-below.tsx
@@ -1,5 +1,12 @@
 export const AddRowBelowIcon = () => (
-  <svg enableBackground="new 0 0 20 20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+  <svg
+    enableBackground="new 0 0 20 20"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+  >
     <title>Legg til rad under</title>
     <path
       clipRule="evenodd"
