be0f0cfa5f02c864d182fd5ac13049de2e07af42
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-08-11 15:54:47 +0200

    Fix overflow in search page

--- frontend/src/pages/search/search.tsx
+++ frontend/src/pages/search/search.tsx
@@ -2,7 +2,7 @@ import { OppgaveSearch } from '@app/components/search/oppgave-search';
 import { VStack } from '@navikt/ds-react';
 
 export const SearchPage = () => (
-  <VStack flexGrow="1" width="100%" overflow="hidden">
+  <VStack flexGrow="1" width="100%" overflow="auto">
     <OppgaveSearch />
   </VStack>
 );
