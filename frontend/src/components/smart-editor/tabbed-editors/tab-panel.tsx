bd512a9d80fbcd93c79d5d5a367136f839a73c6d
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-08-21 13:14:25 +0200

    Remove unused code

--- frontend/src/components/smart-editor/tabbed-editors/tab-panel.tsx
+++ frontend/src/components/smart-editor/tabbed-editors/tab-panel.tsx
@@ -1,11 +1,9 @@
 import { SmartEditorContextComponent } from '@app/components/smart-editor/context';
-import { useHasWriteAccess } from '@app/components/smart-editor/hooks/use-has-write-access';
 import { Editor } from '@app/components/smart-editor/tabbed-editors/editor';
 import { ScalingGroup } from '@app/hooks/settings/use-setting';
 import { ScaleContextComponent } from '@app/plate/status-bar/scale-context';
 import type { ISmartDocumentOrAttachment } from '@app/types/documents/documents';
 import { Tabs } from '@navikt/ds-react';
-import { useEffect, useRef } from 'react';
 
 interface TabPanelProps {
   smartDocument: ISmartDocumentOrAttachment;
@@ -13,21 +11,6 @@ interface TabPanelProps {
 
 export const TabPanel = ({ smartDocument }: TabPanelProps) => {
   const { id } = smartDocument;
-  const smartDocumentRef = useRef<ISmartDocumentOrAttachment>(smartDocument);
-  const hasWriteAccess = useHasWriteAccess(smartDocument);
-  const canEditDocumentRef = useRef(hasWriteAccess);
-
-  // Ensure that smartDocumentRef and canEditDocumentRef are always up to date in order to avoid the unmount debounce triggering on archive/delete/fradeling
-  useEffect(() => {
-    const setRefs = () => {
-      smartDocumentRef.current = smartDocument;
-      canEditDocumentRef.current = hasWriteAccess;
-    };
-
-    setRefs();
-
-    return setRefs;
-  }, [hasWriteAccess, smartDocument]);
 
   return (
     <Tabs.Panel className="h-full overflow-hidden" value={smartDocument.id}>
