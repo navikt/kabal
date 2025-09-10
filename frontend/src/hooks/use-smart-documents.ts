4e25f903d37aa7b858f97f3465e0453a7cbaf091
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-09-10 09:11:33 +0200

    Delete unused code

--- frontend/src/hooks/use-smart-documents.ts
+++
@@ -1,9 +0,0 @@
-import { useGetDocumentsQuery } from '@app/redux-api/oppgaver/queries/documents';
-import type { ISmartDocumentOrAttachment } from '@app/types/documents/documents';
-import type { skipToken } from '@reduxjs/toolkit/query';
-
-export const useSmartDocuments = (oppgaveId: string | typeof skipToken): ISmartDocumentOrAttachment[] | undefined => {
-  const { data } = useGetDocumentsQuery(oppgaveId);
-
-  return data?.filter((d) => d.isSmartDokument).toSorted((a, b) => a.created.localeCompare(b.created));
-};
