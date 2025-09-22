5c532c2c0bf6325e553f912c6928da742098c66a
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-22 10:27:36 +0200

    Revert "Plate: Add markdown plugin (fixes pasting lists from locally installed MS Word)" (broke pasting paragraphs)
    This reverts commit b3ad9d26b5d524bb02623c88c124ad19b9490b83.

--- frontend/src/plate/plugins/plugin-sets/default.ts
+++ frontend/src/plate/plugins/plugin-sets/default.ts
@@ -20,7 +20,6 @@ import { NodeIdPlugin } from '@platejs/core';
 import { DocxPlugin } from '@platejs/docx';
 import { IndentPlugin } from '@platejs/indent/react';
 import { BulletedListPlugin, ListPlugin, NumberedListPlugin } from '@platejs/list-classic/react';
-import { MarkdownPlugin } from '@platejs/markdown';
 import { TableCellPlugin, TablePlugin, TableRowPlugin } from '@platejs/table/react';
 import { ExitBreakPlugin } from '@platejs/utils';
 import { type NodeEntry, ParserPlugin } from 'platejs';
@@ -94,7 +93,7 @@ export const defaultPlugins = [
     },
   }),
   DocxPlugin,
-  MarkdownPlugin,
+
   // Custom plugins
   PageBreakPlugin,
   ProhibitDeletionPlugin,
