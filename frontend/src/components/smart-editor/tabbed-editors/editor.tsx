3781cd76679580b15acf79154a61e87af4798c55
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-10-28 14:27:32 +0100

    Exit placeholder WIP

--- frontend/src/components/smart-editor/tabbed-editors/editor.tsx
+++ frontend/src/components/smart-editor/tabbed-editors/editor.tsx
@@ -71,7 +71,7 @@ const LoadedEditor = ({ oppgave, smartDocument, scalingGroup }: LoadedEditorProp
   const { newCommentSelection } = useContext(SmartEditorContext);
   const { user } = useContext(StaticDataContext);
   const [isConnected, setIsConnected] = useState(false);
-  const [readOnly, setReadOnly] = useState(true); // Start in read-only mode until we know otherwise.
+  const [readOnly, setReadOnly] = useState(false); // TODO: Set to true before merging. // Start in read-only mode until we know otherwise.
 
   const provider: YjsProviderConfig = {
     type: 'hocuspocus',
--- frontend/src/plate/plugins/placeholder/saksbehandler.ts
+++ frontend/src/plate/plugins/placeholder/saksbehandler.ts
@@ -2,12 +2,14 @@ import { isMetaKey } from '@app/keys';
 import { SaksbehandlerPlaceholder } from '@app/plate/components/placeholder/placeholder';
 import { handleArrows } from '@app/plate/plugins/placeholder/arrows';
 import { parsers } from '@app/plate/plugins/placeholder/html-parsers';
+import { isPlaceholderInMaltekst } from '@app/plate/plugins/placeholder/queries';
 import { handleSelectAll } from '@app/plate/plugins/placeholder/select-all';
+import { isOfElementType } from '@app/plate/utils/queries';
 import { createPlatePlugin, type PlateEditor } from '@platejs/core/react';
-import { ElementApi, type NodeEntry } from 'platejs';
+import { ElementApi, NodeApi, type NodeEntry, TextApi } from 'platejs';
 import type { BasePoint } from 'slate';
 import type { PlaceholderElement } from '../../types';
-import { ELEMENT_PLACEHOLDER } from '../element-types';
+import { ELEMENT_MALTEKST, ELEMENT_PLACEHOLDER } from '../element-types';
 import { withOverrides } from './with-overrides';
 
 export const SaksbehandlerPlaceholderPlugin = createPlatePlugin({
@@ -21,6 +23,67 @@ export const SaksbehandlerPlaceholderPlugin = createPlatePlugin({
   handlers: {
     onKeyDown: ({ editor, event }) => {
       if (handleSelectAll(editor, event) || handleArrows(editor, event)) {
+        return;
+      }
+
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const current = editor.api.node({ match: { type: ELEMENT_PLACEHOLDER } });
+        if (current === undefined) {
+          return;
+        }
+
+        const [, currentPath] = current;
+
+        // Find and go to next editable node.
+
+        const next = editor.api.next({
+          at: currentPath,
+          mode: 'lowest',
+          match: (n, p) => {
+            if (!TextApi.isText(n)) {
+              return false;
+            }
+
+            isPlaceholderInMaltekst;
+
+            const ancestors = NodeApi.ancestors(editor, p);
+
+            for (const [ancestor] of ancestors) {
+              if (isOfElementType(ancestor, ELEMENT_MALTEKST)) {
+                return false;
+              }
+            }
+
+            return true;
+          },
+        });
+
+        if (next === undefined) {
+          return;
+        }
+
+        const [_nextNode, nextPath] = next;
+
+        const nextStartPoint = editor.api.start(nextPath);
+
+        if (nextStartPoint === undefined) {
+          return;
+        }
+
+        editor.tf.select(nextStartPoint, { next: true });
+
+        console.table({
+          currentPath: currentPath.join(','),
+          nextStartPointPath: nextStartPoint.path.join(','),
+          nextStartPointOffset: nextStartPoint.offset,
+          path: editor.selection?.focus.path.join(','),
+          offset: editor.selection?.focus.offset,
+          node: JSON.stringify(editor.api.node({ at: editor.selection?.focus, mode: 'lowest' })?.[0], null, 2),
+        });
+
         return;
       }
 
