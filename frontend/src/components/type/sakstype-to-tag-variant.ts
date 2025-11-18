20e866113fd1176fbbd99733330c66bfc9abe3b4
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-11-18 13:24:30 +0100

    Adjust sakstype colors

--- frontend/src/components/type/sakstype-to-tag-variant.ts
+++ frontend/src/components/type/sakstype-to-tag-variant.ts
@@ -8,5 +8,5 @@ export const SAKSTYPE_TO_TAG_VARIANT: Record<SaksTypeEnum, TagProps['variant']> 
   [SaksTypeEnum.OMGJØRINGSKRAV]: 'info-filled',
   [SaksTypeEnum.BEHANDLING_ETTER_TR_OPPHEVET]: 'alt2-filled',
   [SaksTypeEnum.BEGJÆRING_OM_GJENOPPTAK]: 'warning-filled',
-  [SaksTypeEnum.BEGJÆRING_OM_GJENOPPTAK_I_TR]: 'alt3-filled',
+  [SaksTypeEnum.BEGJÆRING_OM_GJENOPPTAK_I_TR]: 'neutral-filled',
 };
