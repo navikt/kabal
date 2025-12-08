1adc74bb37512d7a4bc190b55886684426226995
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-12-08 16:10:18 +0100

    Combine access notification types

--- frontend/src/components/header/notifications/overview/grouped-by-type.tsx
+++ frontend/src/components/header/notifications/overview/grouped-by-type.tsx
@@ -1,24 +1,31 @@
 import { GroupContainer, GroupedGrid, NotificationsGroup } from '@app/components/header/notifications/overview/common';
-import {
-  type KabalNotification,
-  NOTIFICATION_TYPE_LABELS,
-  NOTIFICATION_TYPES,
-} from '@app/components/header/notifications/types';
+import { type KabalNotification, NotificationType } from '@app/components/header/notifications/types';
 import { Heading } from '@navikt/ds-react';
 
 interface GroupedNotificationsProps {
   notifications: readonly KabalNotification[];
 }
 
+interface Column {
+  heading: string;
+  types: NotificationType[];
+}
+
+const COLUMNS: Column[] = [
+  { heading: 'System', types: [NotificationType.SYSTEM] },
+  { heading: 'Tilgang', types: [NotificationType.LOST_ACCESS, NotificationType.GAINED_ACCESS] },
+  { heading: 'Meldinger', types: [NotificationType.MESSAGE] },
+];
+
 export const GroupedByTypeNotifications = ({ notifications }: GroupedNotificationsProps) => (
-  <GroupedGrid columns={NOTIFICATION_TYPES.length}>
-    {NOTIFICATION_TYPES.map((t) => {
-      const filtered = notifications.filter((n) => n.type === t);
+  <GroupedGrid columns={COLUMNS.length}>
+    {COLUMNS.map(({ heading, types }) => {
+      const filtered = notifications.filter((n) => types.includes(n.type));
 
       return (
-        <GroupContainer key={t}>
+        <GroupContainer key={types.join('-')}>
           <Heading level="1" size="xsmall" spacing className="px-2">
-            {NOTIFICATION_TYPE_LABELS[t]}
+            {heading}
           </Heading>
 
           <NotificationsGroup notifications={filtered} showCaseData />
