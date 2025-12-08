16a9802f8422435882beb4c184fe986addc12ce2
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-12-08 16:16:28 +0100

    Improve notifications type columns logic

--- frontend/src/components/header/notifications/overview/grouped-by-type.tsx
+++ frontend/src/components/header/notifications/overview/grouped-by-type.tsx
@@ -6,31 +6,34 @@ interface GroupedNotificationsProps {
   notifications: readonly KabalNotification[];
 }
 
-interface Column {
-  heading: string;
-  types: NotificationType[];
-}
-
-const COLUMNS: Column[] = [
-  { heading: 'System', types: [NotificationType.SYSTEM] },
-  { heading: 'Tilgang', types: [NotificationType.LOST_ACCESS, NotificationType.GAINED_ACCESS] },
-  { heading: 'Meldinger', types: [NotificationType.MESSAGE] },
-];
-
 export const GroupedByTypeNotifications = ({ notifications }: GroupedNotificationsProps) => (
-  <GroupedGrid columns={COLUMNS.length}>
-    {COLUMNS.map(({ heading, types }) => {
-      const filtered = notifications.filter((n) => types.includes(n.type));
-
-      return (
-        <GroupContainer key={types.join('-')}>
-          <Heading level="1" size="xsmall" spacing className="px-2">
-            {heading}
-          </Heading>
-
-          <NotificationsGroup notifications={filtered} showCaseData />
-        </GroupContainer>
-      );
-    })}
+  <GroupedGrid columns={3}>
+    <Column heading="System" types={[NotificationType.SYSTEM]} notifications={notifications} />
+    <Column
+      heading="Tilgang"
+      types={[NotificationType.LOST_ACCESS, NotificationType.GAINED_ACCESS]}
+      notifications={notifications}
+    />
+    <Column heading="Meldinger" types={[NotificationType.MESSAGE]} notifications={notifications} />
   </GroupedGrid>
 );
+
+interface ColumnProps {
+  heading: string;
+  types: NotificationType[];
+  notifications: readonly KabalNotification[];
+}
+
+const Column = ({ heading, types, notifications }: ColumnProps) => {
+  const filtered = notifications.filter((n) => types.includes(n.type));
+
+  return (
+    <GroupContainer>
+      <Heading level="1" size="xsmall" spacing className="px-2">
+        {heading}
+      </Heading>
+
+      <NotificationsGroup notifications={filtered} showCaseData />
+    </GroupContainer>
+  );
+};
