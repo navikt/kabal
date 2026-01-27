6664fbfcf3aba4ae17dbaac876e95921cd13fb2d
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-01-27 14:57:04 +0100

    Only let saksbehandler deselect utfasede innsendingshjemler

--- frontend/src/components/oppgavebehandling-footer/deassign/hjemmel-list.tsx
+++ frontend/src/components/oppgavebehandling-footer/deassign/hjemmel-list.tsx
@@ -1,27 +1,70 @@
+import { isNotUndefined } from '@app/functions/is-not-type-guards';
 import { stringToRegExp } from '@app/functions/string-to-regex';
 import { useKodeverkYtelse } from '@app/hooks/use-kodeverk-value';
-import { Box, Heading, Loader, Search, VStack } from '@navikt/ds-react';
-import { useMemo, useState } from 'react';
+import type { IYtelse } from '@app/types/kodeverk';
+import { Box, ErrorMessage, Heading, Loader, Search, VStack } from '@navikt/ds-react';
+import { useMemo, useRef, useState } from 'react';
 import { Direction } from '../../deassign/direction';
 import { FilterList } from '../../filter-dropdown/filter-list';
 
-interface Props {
+interface CommonProps {
   selected: string[];
   onChange: (hjemler: string[]) => void;
-  ytelseId: string;
   direction: Direction;
   error: string | null;
 }
 
-export const HjemmelList = ({ selected, ytelseId, direction, onChange, error }: Props) => {
-  const [ytelse, ytelseIsLoading] = useKodeverkYtelse(ytelseId);
+export const HjemmelList = ({ ytelseId, ...props }: CommonProps & { ytelseId: string }) => {
+  const [ytelse, isLoading] = useKodeverkYtelse(ytelseId);
+
+  if (isLoading) {
+    return (
+      <VStack>
+        <Loader title="Laster..." className="m-2" />
+      </VStack>
+    );
+  }
+
+  if (ytelse === undefined) {
+    return <ErrorMessage>Kunne ikke finne ytelse med id: {ytelseId}</ErrorMessage>;
+  }
+
+  return <HjemmelListContent {...props} ytelse={ytelse} />;
+};
+
+export const HjemmelListContent = ({
+  selected,
+  direction,
+  onChange,
+  error,
+  ytelse,
+}: CommonProps & { ytelse: IYtelse }) => {
   const [search, setSearch] = useState('');
   const filterRegex = useMemo(() => stringToRegExp(search), [search]);
 
-  const options = useMemo(
-    () => ytelse?.innsendingshjemler.map(({ id, navn }) => ({ value: id, label: navn })) ?? [],
-    [ytelse?.innsendingshjemler],
+  const selectedUtfasesIds = useRef(
+    selected.filter((s) => ytelse.innsendingshjemler.find((h) => h.id === s)?.utfases === true),
   );
+
+  const options = useMemo(() => {
+    const selectedUtfasesOptions = selectedUtfasesIds.current
+      .map((id) => {
+        const hjemmel = ytelse.innsendingshjemler.find((h) => h.id === id);
+
+        if (hjemmel === undefined) {
+          return undefined;
+        }
+
+        return { value: hjemmel.id, label: `${hjemmel.navn} (utfases)` };
+      })
+      .filter(isNotUndefined);
+
+    return ytelse.innsendingshjemler
+      .filter(({ utfases }) => !utfases)
+      .map(({ id, navn }) => ({ value: id, label: navn }))
+      .concat(selectedUtfasesOptions)
+      .toSorted((a, b) => a.label.localeCompare(b.label));
+  }, [ytelse.innsendingshjemler]);
 
   const filteredOptions = useMemo(
     () => options.filter((option) => filterRegex.test(option.label)),
@@ -59,17 +102,13 @@ export const HjemmelList = ({ selected, ytelseId, direction, onChange, error }: 
             <hr className="m-0 border-ax-border-neutral border-b" />
           </>
         ) : null}
-        {ytelseIsLoading ? (
-          <Loader title="Laster..." className="m-2" />
-        ) : (
-          <FilterList
-            options={filteredOptions}
-            selected={selected}
-            onChange={onChange}
-            error={error}
-            className="max-h-[min(50vh,400px)] overflow-y-auto"
-          />
-        )}
+        <FilterList
+          options={filteredOptions}
+          selected={selected}
+          onChange={onChange}
+          error={error}
+          className="max-h-[min(50vh,400px)] overflow-y-auto"
+        />
       </Box>
     </VStack>
   );
