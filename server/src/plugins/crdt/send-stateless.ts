42c95185914df72a09bfd4b9410d5fd73498d2ae
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-02-27 10:41:09 +0100

    Ensure RTKQ store is updated after smart document is removed + log more metadata

---
+++ server/src/plugins/crdt/send-stateless.ts
@@ -0,0 +1,27 @@
+import { getLogger } from '@app/logger';
+import type { Connection } from '@hocuspocus/server';
+
+const OLD_VERSION = '2026-02-25T09:43:48';
+
+const log = getLogger('send-stateless');
+
+export const sendStateless = (
+  sendStateless: Connection['sendStateless'],
+  client_version: string | undefined,
+  type: string,
+  metadata: { trace_id: string | undefined; span_id: string | undefined; tab_id: string | undefined },
+) => {
+  if (client_version === undefined) {
+    log.error({ msg: 'Client version is undefined', ...metadata, data: { type } });
+
+    return;
+  }
+
+  if (client_version <= OLD_VERSION) {
+    sendStateless(type);
+
+    return;
+  }
+
+  sendStateless(JSON.stringify({ type, ...metadata }));
+};
