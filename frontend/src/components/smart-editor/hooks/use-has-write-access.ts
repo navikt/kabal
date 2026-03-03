f151bb1050e9337281d3a455feef1993b62fbd1c
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-03-03 15:43:25 +0100

    Hide reply button if user has no access to reply

--- frontend/src/components/smart-editor/hooks/use-has-write-access.ts
+++ frontend/src/components/smart-editor/hooks/use-has-write-access.ts
@@ -4,13 +4,18 @@ import { useParentDocument } from '@app/hooks/use-parent-document';
 import type { ISmartDocumentOrAttachment } from '@app/types/documents/documents';
 import { useMemo } from 'react';
 
-export const useHasWriteAccess = ({ parentId, creator, type, templateId }: ISmartDocumentOrAttachment): boolean => {
-  const parent = useParentDocument(parentId);
+export const useHasWriteAccess = (props: ISmartDocumentOrAttachment | null): boolean => {
+  const parent = useParentDocument(props?.parentId ?? null);
   const getDuaAccess = useLazyDuaAccess();
 
   return useMemo(() => {
+    if (props === null) {
+      return false;
+    }
+
+    const { creator, type, templateId } = props;
     const writeAccessError = getDuaAccess({ creator, type, templateId }, DuaActionEnum.WRITE, parent);
 
     return writeAccessError === null;
-  }, [creator, type, templateId, getDuaAccess, parent]);
+  }, [props, getDuaAccess, parent]);
 };
