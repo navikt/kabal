66409667b7e0fdd6a52633166676e0a61b9f9d67
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-03-04 15:47:22 +0100

    Remove deprecated ekspedisjonsbrev til TR feature toogle - it is now globally enabled

--- frontend/src/simple-api-state/feature-toggles.ts
+++
@@ -1,9 +0,0 @@
-import { SimpleApiState, useSimpleApiState } from '@app/simple-api-state/simple-api-state';
-
-type FeatureToggle = {
-  enabled: boolean;
-};
-
-const createEkspedisjonsbrevToTR = new SimpleApiState<FeatureToggle>('/feature-toggle/createEkspedisjonsbrevToTR');
-
-export const useCreateEkspedisjonsbrevToTRFeatureToggle = () => useSimpleApiState(createEkspedisjonsbrevToTR);
