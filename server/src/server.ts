2b33380d4c0e7c255931a4b44e542f06b1869fa9
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-03-11 13:39:37 +0100

    Set max param length to 20_000

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,7 +44,7 @@ if (isDeployed) {
 
 const bodyLimit = 300 * 1024 * 1024; // 300 MB
 
-fastify({ trustProxy: true, bodyLimit, routerOptions: { querystringParser } })
+fastify({ trustProxy: true, bodyLimit, maxParamLength: 20_000, routerOptions: { querystringParser } })
   .register(fastifyWebsocket)
   .register(cors, corsOptions)
   .register(healthPlugin)
