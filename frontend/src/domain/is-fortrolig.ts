6275442e2fd32d54247d1ce49c009aa193e4c5ed
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-04-08 15:49:20 +0200

    Treat strengt fortrolig like fortrolig

--- frontend/src/domain/is-fortrolig.ts
+++ frontend/src/domain/is-fortrolig.ts
@@ -1,7 +1,7 @@
 import { type IPersonStatus, type ISakenGjelder, PartStatusEnum } from '@/types/oppgave-common';
 
 export const hasFortroligStatus = (statusList: IPersonStatus[]) =>
-  statusList.some(({ status }) => status === PartStatusEnum.FORTROLIG);
+  statusList.some(({ status }) => status === PartStatusEnum.FORTROLIG || status === PartStatusEnum.STRENGT_FORTROLIG);
 
 export const hasFortroligFamily = ({ protectedFamilyMembers }: ISakenGjelder) =>
   protectedFamilyMembers.some(({ statusList }) => hasFortroligStatus(statusList));
