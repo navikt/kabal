16f05f6dbdeeb8ea11d4052d257a9774881d1c4c
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-04-22 18:21:11 +0200

    Fix new comment keyboard shortcut

--- frontend/src/plate/toolbar/add-comment.tsx
+++ frontend/src/plate/toolbar/add-comment.tsx
@@ -1,27 +1,18 @@
 import { ChatElipsisIcon } from '@navikt/aksel-icons';
-import { skipToken } from '@reduxjs/toolkit/query';
 import { RangeApi } from 'platejs';
 import { useContext } from 'react';
-import { StaticDataContext } from '@/components/app/static-data-context';
-import { connectCommentThread } from '@/components/smart-editor/comments/connect-thread';
 import { SmartEditorContext } from '@/components/smart-editor/context';
-import { useOppgaveId } from '@/hooks/oppgavebehandling/use-oppgave-id';
+import { useAddComment } from '@/components/smart-editor/hooks/use-add-comment';
 import { MOD_KEY_TEXT } from '@/keys';
 import { useSelection } from '@/plate/hooks/use-selection';
 import { ToolbarIconButton } from '@/plate/toolbar/toolbarbutton';
-import { useMyPlateEditorRef } from '@/plate/types';
-import { usePostCommentMutation } from '@/redux-api/smart-editor-comments';
-import type { ISmartEditorComment } from '@/types/smart-editor/comments';
 
 export const CommentsButton = () => {
-  const oppgaveId = useOppgaveId();
-  const { setNewCommentSelection, dokumentId, editingComment, setEditingComment } = useContext(SmartEditorContext);
+  const { editingComment } = useContext(SmartEditorContext);
   const selection = useSelection();
-  const [postComment, { isLoading }] = usePostCommentMutation();
-  const { user } = useContext(StaticDataContext);
-  const editor = useMyPlateEditorRef();
+  const { addComment, isLoading, isAvailable } = useAddComment();
 
-  if (oppgaveId === skipToken) {
+  if (!isAvailable) {
     return null;
   }
 
@@ -30,25 +21,7 @@ export const CommentsButton = () => {
       label="Legg til kommentar"
       keys={[MOD_KEY_TEXT, 'Shift', 'K']}
       icon={<ChatElipsisIcon width={24} />}
-      onClick={async () => {
-        if (selection === null || RangeApi.isCollapsed(selection)) {
-          return;
-        }
-
-        const comment: ISmartEditorComment = await postComment({
-          author: { ident: user.navIdent, name: user.navn },
-          dokumentId,
-          oppgaveId,
-          text: '',
-        }).unwrap();
-
-        connectCommentThread(editor, selection, comment.id);
-
-        setTimeout(() => {
-          setNewCommentSelection(selection);
-          setEditingComment(comment);
-        }, 50);
-      }}
+      onClick={() => addComment(selection)}
       active={editingComment !== null}
       disabled={selection === null || RangeApi.isCollapsed(selection)}
       loading={isLoading}
