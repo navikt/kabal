c02ae8cd60d5263f77f7a1db664526000a809bf7
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-05-22 16:52:16 +0200

    Align Hjemler filename with component name

--- frontend/src/components/common-table-components/oppgave-table/filter-dropdowns/user-hjemler.tsx
+++ frontend/src/components/common-table-components/oppgave-table/filter-dropdowns/user-hjemler.tsx
@@ -1,4 +1,4 @@
-import { Hjemler } from '@/components/common-table-components/oppgave-table/filter-dropdowns/hjemmel';
+import { Hjemler } from '@/components/common-table-components/oppgave-table/filter-dropdowns/hjemler';
 import type { FilterDropdownProps } from '@/components/common-table-components/oppgave-table/filter-dropdowns/types';
 import { useSettingsHjemler } from '@/hooks/use-settings-hjemler';
 
