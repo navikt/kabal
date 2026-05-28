96144b9960b51e792a34fe28750a5eb655dcbc23
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-05-28 14:39:07 +0200

    Rename Hjemler to Innsendingshjemler

--- frontend/src/components/common-table-components/oppgave-table/filter-dropdowns/all-innsendingshjemler.tsx
+++ frontend/src/components/common-table-components/oppgave-table/filter-dropdowns/all-innsendingshjemler.tsx
@@ -1,9 +1,9 @@
-import { Hjemler } from '@/components/common-table-components/oppgave-table/filter-dropdowns/hjemler';
+import { Innsendingshjemler } from '@/components/common-table-components/oppgave-table/filter-dropdowns/innsendingshjemler';
 import type { FilterDropdownProps } from '@/components/common-table-components/oppgave-table/filter-dropdowns/types';
 import { useAllActiveHjemler } from '@/hooks/use-all-active-hjemler';
 
 export const AllInnsendingshjemler = (props: FilterDropdownProps) => {
   const hjemler = useAllActiveHjemler();
 
-  return <Hjemler {...props} hjemler={hjemler} />;
+  return <Innsendingshjemler {...props} hjemler={hjemler} />;
 };
