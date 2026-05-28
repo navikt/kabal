0c4b431db41fa9c6342f1e35d2685aa5a29c8c2a
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-05-28 14:26:13 +0200

    Upgrade settings and available ytelser hooks to return full IYtelse

--- frontend/src/hooks/use-settings-hjemler.ts
+++ frontend/src/hooks/use-settings-hjemler.ts
@@ -1,14 +1,30 @@
+import { useSettingsYtelser } from '@/hooks/use-settings-ytelser';
 import { useGetSettingsQuery } from '@/redux-api/bruker';
-import { useHjemler } from '@/simple-api-state/use-kodeverk';
-import type { IKodeverkValue } from '@/types/kodeverk';
+import type { IYtelseHjemmel } from '@/types/kodeverk';
 
-export const useSettingsHjemler = (): IKodeverkValue[] => {
-  const { data: hjemler } = useHjemler();
+export const useSettingsHjemler = (): IYtelseHjemmel[] => {
+  const userYtelser = useSettingsYtelser();
   const { data: settings, isSuccess } = useGetSettingsQuery();
 
-  if (!isSuccess || hjemler === undefined) {
+  if (!isSuccess) {
     return [];
   }
 
-  return hjemler.filter(({ id }) => settings.hjemler.includes(id));
+  const hjemler: IYtelseHjemmel[] = [];
+
+  for (const { innsendingshjemler } of userYtelser) {
+    for (const hjemmel of innsendingshjemler) {
+      if (!settings.hjemler.includes(hjemmel.id)) {
+        continue;
+      }
+
+      if (hjemler.some(({ id }) => id === hjemmel.id)) {
+        continue;
+      }
+
+      hjemler.push(hjemmel);
+    }
+  }
+
+  return hjemler;
 };
