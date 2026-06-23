bd9ace5c8e70035984c23d79a2ff627a66c145d3
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-06-23 09:12:26 +0200

    Saksinfo: Make element easy to locate for e2e + simplify

--- frontend/src/plate/components/saksinfo.tsx
+++ frontend/src/plate/components/saksinfo.tsx
@@ -3,21 +3,18 @@ import { formatLongDate } from '@/domain/date';
 import { pxToEm } from '@/plate/components/get-scaled-em';
 import type { SaksinfoElement } from '@/plate/types';
 
-export const Saksinfo = (props: PlateElementProps<SaksinfoElement>) => {
-  const { children, ...rest } = props;
-
-  return (
-    <PlateElement<SaksinfoElement> {...rest} as="div">
-      <div
-        className="flex items-end justify-between"
-        style={{ marginTop: pxToEm(48), marginBottom: pxToEm(48), fontSize: pxToEm(11) }}
-      >
-        <div>{children}</div>
-        <span>{formatLongDate(YEAR, MONTH, DAY)}</span>
-      </div>
-    </PlateElement>
-  );
-};
+export const Saksinfo = ({ children, ...rest }: PlateElementProps<SaksinfoElement>) => (
+  <PlateElement<SaksinfoElement>
+    {...rest}
+    attributes={{ ...rest.attributes, 'data-element': rest.element.type }}
+    as="div"
+    className="flex items-end justify-between"
+    style={{ marginTop: pxToEm(48), marginBottom: pxToEm(48), fontSize: pxToEm(11) }}
+  >
+    <div>{children}</div>
+    <span>{formatLongDate(YEAR, MONTH, DAY)}</span>
+  </PlateElement>
+);
 
 const NOW = new Date();
 const YEAR = NOW.getFullYear();
