a6c3ac1305bb7c4371d13c1052b71931a2d90775
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-07-22 14:20:28 +0200

    Show and validate TR fields for all ekspedisjonsbrev

--- frontend/src/components/documents/new-documents/has-tr-fields.ts
+++ frontend/src/components/documents/new-documents/has-tr-fields.ts
@@ -1,13 +1,4 @@
 import { DistribusjonsType, type IParentDocument } from '@/types/documents/documents';
-import { TemplateIdEnum } from '@/types/smart-editor/template-enums';
 
-export const hasTrFields = ({ dokumentTypeId, templateId }: IParentDocument) => {
-  if (dokumentTypeId !== DistribusjonsType.EKSPEDISJONSBREV_TIL_TRYGDERETTEN) {
-    return false;
-  }
-
-  return (
-    templateId === TemplateIdEnum.EKSPEDISJONSBREV_TIL_TRYGDERETTEN ||
-    templateId === TemplateIdEnum.GJENOPPTAKSBEGJÆRING_EKSPEDISJONSBREV_TIL_TR
-  );
-};
+export const hasTrFields = ({ dokumentTypeId }: Pick<IParentDocument, 'dokumentTypeId'>) =>
+  dokumentTypeId === DistribusjonsType.EKSPEDISJONSBREV_TIL_TRYGDERETTEN;
