86c575660dcaec86ecc7b0f89d186d7ac6b45919
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-08-13 12:52:06 +0200

    Change confirm checkbox text for some Arena cases

--- frontend/src/components/oppgavebehandling-footer/confirm-finish/confirm-finish-info.tsx
+++ frontend/src/components/oppgavebehandling-footer/confirm-finish/confirm-finish-info.tsx
@@ -29,8 +29,22 @@ interface ArenaConfirmationCheckboxProps {
 
 export const ArenaConfirmationCheckbox = ({ typeId, confirmed, setConfirmed }: ArenaConfirmationCheckboxProps) => (
   <Checkbox onChange={(e) => setConfirmed(e.target.checked)} checked={confirmed} size="small">
-    {typeId === SaksTypeEnum.ANKE_I_TRYGDERETTEN
-      ? 'Jeg bekrefter at jeg har registrert utfallet fra Trygderetten i Arena'
-      : 'Jeg bekrefter at saken er besluttet i Arena.'}
+    {getArenaCheckboxText(typeId)}
   </Checkbox>
 );
+
+const getArenaCheckboxText = (typeId: SaksTypeEnum) => {
+  if (typeId === SaksTypeEnum.ANKE_I_TRYGDERETTEN) {
+    return 'Jeg bekrefter at jeg har registrert utfallet fra Trygderetten i Arena';
+  }
+
+  if (
+    typeId === SaksTypeEnum.KLAGE ||
+    typeId === SaksTypeEnum.ANKE ||
+    typeId === SaksTypeEnum.BEHANDLING_ETTER_TR_OPPHEVET
+  ) {
+    return 'Jeg bekrefter at saken er sendt til godkjenning i Arena.';
+  }
+
+  return 'Jeg bekrefter at saken er besluttet i Arena.';
+};
