2bc54acf499367a993d7af325e8c2ae8bd7b8b1f
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-09-03 14:09:47 +0200

    Treat Gosys same as Arbeidsoppfølging

--- frontend/src/domain/treat-as-arena.ts
+++ frontend/src/domain/treat-as-arena.ts
@@ -1,4 +1,19 @@
-import { FAGSYSTEM_ARBEIDSOPPFØLGING, FAGSYSTEM_ARENA } from '@/components/oppgavebehandling-footer/fagsystem';
+import {
+  FAGSYSTEM_ARBEIDSOPPFØLGING,
+  FAGSYSTEM_ARENA,
+  FAGSYSTEM_GOSYS,
+} from '@/components/oppgavebehandling-footer/fagsystem';
 
-export const treatAsArena = (fagsystemId: string, requiresGosysOppgave: boolean) =>
-  fagsystemId === FAGSYSTEM_ARENA || (fagsystemId === FAGSYSTEM_ARBEIDSOPPFØLGING && requiresGosysOppgave);
+// https://nav-it.slack.com/archives/G01CTUC8LSU/p1787141984237739
+// https://nav-it.slack.com/archives/G01CTUC8LSU/p1788425945624739
+export const treatAsArena = (fagsystemId: string, requiresGosysOppgave: boolean) => {
+  if (fagsystemId === FAGSYSTEM_ARENA) {
+    return true;
+  }
+
+  if (requiresGosysOppgave) {
+    return fagsystemId === FAGSYSTEM_ARBEIDSOPPFØLGING || fagsystemId === FAGSYSTEM_GOSYS;
+  }
+
+  return false;
+};
